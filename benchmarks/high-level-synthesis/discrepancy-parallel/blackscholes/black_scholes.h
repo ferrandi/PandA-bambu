29ba3ccd98d8c24903ea0d26b92fc2ad07599366
Pietro Fezzardi <pietrofezzardi@gmail.com>
2017-05-22 17:09:15 +0200

    further reduce blackscholes benchmark
    Former-commit-id: 2f3f599a879674e592e8b0a5e59db450f4d540a1

--- benchmarks/high-level-synthesis/discrepancy-parallel/blackscholes/black_scholes.h
+++ benchmarks/high-level-synthesis/discrepancy-parallel/blackscholes/black_scholes.h
@@ -14,8 +14,4 @@
 fixedpt asset_path_fixed_simplified ( fixedpt s0, fixedpt mu, fixedpt sigma, fixedpt t1, int n, int *seed);
 void get_two_normal_fixed(int *seed, fixedpt *n1, fixedpt *n2);
 fixedpt get_uniform_fixed ( int *seed );
-
-
-
 #endif  /* BLACK_SCHOLES_H */
-
--- benchmarks/high-level-synthesis/discrepancy-parallel/blackscholes/main.c
+++ benchmarks/high-level-synthesis/discrepancy-parallel/blackscholes/main.c
@@ -8,7 +8,6 @@
 
 #include "black_scholes.h"
 #include "fixedptc.h"
-#include "seed.h"
 
 fixedpt asset_path_test(int seed)
 {
@@ -17,20 +16,51 @@ fixedpt asset_path_test(int seed)
   fixedpt mu = 16384; // fixedpt_rconst(0.25);
   fixedpt sigma = 4391; //fixedpt_rconst(0.067);
   fixedpt t1 = 131072; // fixedpt_rconst(2.0);
-  fixedpt s = asset_path_fixed_simplified(s0, mu, sigma, t1, n, &seed);
-  return s;
+  return asset_path_fixed_simplified(s0, mu, sigma, t1, n, &seed);
 }
 
 fixedpt black_scholes()
 {
   int run = RUN;
   fixedpt sum = 0;
+  fixedpt seeds[RUN] = {
+    35645,
+    20955,
+    3231,
+    55398,
+    11191,
+    7730,
+    41421,
+    52573,
+    46468,
+    4465,
+    20791,
+    64790,
+    59818,
+    42394,
+    12807,
+    36702,
+    28255,
+    17195,
+    64055,
+    15204,
+    12488,
+    45439,
+    10215,
+    52185,
+    12464,
+    30711,
+    11892,
+    54818,
+    28299,
+    29363,
+    22002,
+    49795,
+  };
   for (int i = 0; i < run; i++) {
-    fixedpt u;
     fixedpt seed = seeds[i];
-    u = asset_path_test (seed);
-    result[i] = u;
-    sum += (u >> (FIXEDPT_BITS - FIXEDPT_WBITS));
+    fixedpt u = asset_path_test(seed);
+    sum += (u >> FIXEDPT_BITS - FIXEDPT_WBITS);
   }
   return sum;
 }
