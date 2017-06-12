386a9409306627285d0a0a389efb5cc50e54ab3a
Pietro Fezzardi <pietrofezzardi@gmail.com>
2017-06-12 11:57:15 +0200

    update script for hw-discrepancy regressions

--- panda_regressions/hls/hw-discrepancy.sh
+++ panda_regressions/hls/hw-discrepancy.sh
@@ -1,12 +1,12 @@
-+#!/bin/bash
-`dirname $0`/../../etc/scripts/test_panda.py --tool=bambu \
-             --args="--configuration-name=without --clock-period=5 --evaluation --device=5SGXEA7N2F45C1 --fsm-encoding=binary" \
-     --args="--configuration-name=with --clock-period=5 --evaluation --device=5SGXEA7N2F45C1 --fsm-encoding=binary --discrepancy-hw" \
-     -lCHStone_list -o output_hw_discrepancy -b`dirname $0` --table=table_hw_discrepancy.tex --name="hw_discrepancy" $@ \
-     --spider-style="$root_dir/latex_format_bambu_results_altera_synth.xml"
+#!/bin/bash
+abs_script=`readlink -e $0`
+dir_script=`dirname $abs_script`
+$dir_script/../../etc/scripts/test_panda.py --tool=bambu \
+     --args="--debug-classes=HWDiscrepancyAnalysis --generate-vcd --configuration-name=with --aligned -fwhole-program -fno-delete-null-pointer-checks --no-iob --clock-period=5 --evaluation --device=5SGXEA7N2F45C1 --fsm-encoding=binary --discrepancy-hw" \
+     --args="--generate-vcd --configuration-name=without --aligned -fwhole-program -fno-delete-null-pointer-checks --no-iob --clock-period=5 --evaluation --device=5SGXEA7N2F45C1 --fsm-encoding=binary" \
+     -l$dir_script/CHStone_list -o output_hw_discrepancy -b$dir_script --table=table_hw_discrepancy.tex --name="hw_discrepancy" $@
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
 fi
 exit 0
-
