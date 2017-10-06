00d5d068e25bde1362c476ab85943a4d8f585335
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2017-10-06 18:10:45 +0200

    * Update the regression scripts.

---
+++ panda_regressions/hls/panda_regression_hls_all_gcc1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+$(dirname $0)/gcc_regression_simple1.sh $@
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
+$(dirname $0)/softfloat-tests1.sh $@
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
+if [[ "$@" != *"-wH"* ]] && [[ "$@" != *"--speculative-sdc-scheduling"* ]] ;  then
+   $(dirname $0)/discrepancy.sh $@
+   return_value=$?
+   if test $return_value != 0; then
+      exit $return_value
+   fi
+fi
+#$(dirname $0)/wb4_interface.sh $@
+#return_value=$?
+#if test $return_value != 0; then
+#   exit $return_value
+#fi
+exit 0
---
+++ panda_regressions/panda_regression_hls1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+abs_script=$(readlink -e $0)
+dir_script=$(dirname $abs_script)
+$dir_script/panda_regression_hls_release1.sh $@
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
+exit 0
