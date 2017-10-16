5d3d8a3098abd51c5c0d9d1c6deb7f94006e6611
Fabrizio Ferrandi <ferrandi@users.noreply.github.com>
2017-10-16 15:02:59 +0900

    Fixes for make check and cpp_examples. (#6)
    * Possible fix to make check and cpp_examples
    
    * Updated 0.9.6 new feature list.

--- examples/cpp_examples/fir_filter/bambu.sh
+++ examples/cpp_examples/fir_filter/bambu.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 script=$(readlink -e $0)
 root_dir=$(dirname $script)
-export PATH=../../src:../../../src:../../../../src:/opt/panda/bin:$PATH
-
-mkdir -p $root_dir/hls1
-cd $root_dir/hls1
+basename_dir=$(basename $root_dir)
+current_dir=$(pwd)
+export PATH=../../src:../../../src:../../../../src:../../../../../src:/opt/panda/bin:$PATH
+mkdir -p $basename_dir
+mkdir -p $basename_dir/hls1
+cd $basename_dir/hls1
 echo "#complete synthesis and simulation"
 bambu -v5 --print-dot $root_dir/coef1.cpp --compiler=I386_GCC5 --no-iob --evaluation --std=c++1y --top-fname=main --top-rtldesign-name=firFixed --file-input-data=$root_dir/input.pcm -v2 -DNDEBUG -DWITH_EXTERNC
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
 fi
-diff $root_dir/hls1/outputFixed.pcm $root_dir/outputFixed.gold.pcm
+diff outputFixed.pcm $root_dir/outputFixed.gold.pcm
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
 fi
-
-mkdir -p $root_dir/hls2
-cd $root_dir/hls2
+cd $current_dir
+mkdir -p $basename_dir/hls2
+cd $basename_dir/hls2
 echo "#focused synthesis and simulation"
 bambu -v5 --print-dot $root_dir/coef1.cpp --compiler=I386_GCC5 --no-iob --evaluation --std=c++1y --top-fname=firFixed --generate-tb=$root_dir/test.xml -v2
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
 fi
-
 exit 0
--- examples/cpp_examples/gcd_example/bambu.sh
+++ examples/cpp_examples/gcd_example/bambu.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 script=$(readlink -e $0)
 root_dir=$(dirname $script)
+basename_dir=$(basename $root_dir)
+
 export PATH=../../src:../../../src:../../../../src:/opt/panda/bin:$PATH
 
-mkdir -p $root_dir/hls
-cd $root_dir/hls
+mkdir -p $basename_dir
+mkdir -p $basename_dir/hls
+cd $basename_dir/hls
 echo "#synthesis and simulation"
-bambu -v5 --print-dot $root_dir/gcd.cc --compiler=I386_GCC49 --generate-tb=$root_dir/test.xml --simulator=VERILATOR --no-iob --evaluation
+bambu -v5 --print-dot $root_dir/gcd.cc --compiler=I386_GCC49 --generate-tb=$root_dir/test.xml --no-iob --evaluation
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
