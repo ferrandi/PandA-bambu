3c1584d9851b7cbe932064638be40ad0e0ad7aa1
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2017-12-12 22:39:10 +0100

    * Added FPT17 introduction material.

---
+++ documentation/tutorial_fpt_2017/intro/third/src/chi/chi.c
@@ -0,0 +1,17 @@
+#include "Keccak.h"
+
+void chi(UINT64 *A)
+{
+    unsigned int x, y;
+    UINT64 C[5];
+
+    for(y=0; y<5; y++) { 
+        for(x=0; x<5; x++)
+            C[x] = A[index(x, y)] ^ ((~A[index(x+1, y)]) & A[index(x+2, y)]);
+        for(x=0; x<5; x++)
+            A[index(x, y)] = C[x];
+    }
+}
+
+
+
