56ac678b802c096808eab284473f1d10b7b48c9c
Marco Lattuada <marco.lattuada@polimi.it>
2018-01-18 16:44:40 +0100

    Added parallel queries benchmarks

---
+++ benchmarks/high-level-synthesis/parallel/parallel_queries/trinityq5/lubm_trinityq5.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include "simple_API.h"
+
+// var_2 = "<http://www.Department0.University0.edu>"
+// p_var_3 = "ub:subOrganizationOf"
+// p_var_4 = "ub:ResearchGroup"
+// p_var_5 = "a"
+
+__attribute__((noinline))
+void kernel(size_t i_var_3, Graph * graph, NodeId var_2, PropertyId p_var_3, PropertyId p_var_4, PropertyId p_var_5, size_t in_degree_var_2, Edge * var_2_1_inEdges)
+{
+  unsigned localCounter = 0;
+  PropertyId var_3;  //corresponding to element having label "ub:subOrganizationOf"
+  var_3 = var_2_1_inEdges[i_var_3].property;
+  NodeId var_1; //corresponding to element having label "?X"
+  var_1 = var_2_1_inEdges[i_var_3].node;
+  int  cond_level_2 = (var_3 == p_var_3);
+  if(cond_level_2)
+  {
+    size_t out_degree_var_1 = getOutDegree(graph, var_1);
+    Edge * var_1_3_outEdges = getOutEdges(graph, var_1);
+    size_t i_var_5;
+    for(i_var_5=0; i_var_5 < out_degree_var_1; i_var_5++)
+    {
+      PropertyId var_5;  //corresponding to element having label "a"
+      var_5 = var_1_3_outEdges[i_var_5].property;
+      NodeId  var_4;   //corresponding to element having label "ub:ResearchGroup"
+      var_4 = var_1_3_outEdges[i_var_5].node;
+      int  cond_level_4 = ((var_5 == p_var_5) & (var_4 == p_var_4));
+      if(cond_level_4)
+      {
+        // here the "required" results are written (if any)
+        localCounter++;
+      }
+    }
+    atomicIncrement(&(counter[i_var_3 % N_THREADS]), localCounter);
+  }
+}
+
+
+__attribute__((noinline))
+void parallel(Graph * graph, NodeId var_2, PropertyId p_var_3, PropertyId p_var_4, PropertyId p_var_5, size_t in_degree_var_2, Edge * var_2_1_inEdges)
+{
+   size_t i_var_3;
+   #pragma omp parallel for
+   for(i_var_3=0; i_var_3 < in_degree_var_2; i_var_3++)
+   {
+     kernel(i_var_3, graph, var_2, p_var_3, p_var_4, p_var_5, in_degree_var_2, var_2_1_inEdges);
+   }
+}
+
+__attribute__((noinline))
+void search(Graph * graph, NodeId var_2, PropertyId p_var_3, PropertyId p_var_4, PropertyId p_var_5)
+{
+#ifndef DEBUG
+   __builtin_bambu_time_start();
+#endif
+   size_t in_degree_var_2 = getInDegree(graph, var_2);
+   printf("In degree %d\n", in_degree_var_2);
+   Edge * var_2_1_inEdges = getInEdges(graph, var_2);
+   parallel(graph, var_2, p_var_3, p_var_4, p_var_5, in_degree_var_2, var_2_1_inEdges);
+
+   for (int i = 0; i < N_THREADS; ++i)
+     numAnswers += counter[i];
+#ifndef DEBUG
+   __builtin_bambu_time_stop();
+#endif
+}
+
+
+int test(NodeId var_2, PropertyId p_var_3, PropertyId p_var_4, PropertyId p_var_5)
+{
+#if defined(DATASETInVertexFile) && defined(DATASETOutVertexFile) && defined(DATASETInEdgeFile) && defined(DATASETOutEdgeFile)
+  loadGraph(DATASETInVertexFile, DATASETOutVertexFile, DATASETInEdgeFile, DATASETOutEdgeFile);
+#else
+  //loadGraph("dataset/40-InVertexFile.bin", "dataset/40-OutVertexFile.bin", "dataset/40-InEdgeFile.bin", "dataset/40-OutEdgeFile.bin");
+  loadGraph("dataset/1-InVertexFile.bin", "dataset/1-OutVertexFile.bin", "dataset/1-InEdgeFile.bin", "dataset/1-OutEdgeFile.bin");
+#endif
+
+// var_2 = "<http://www.Department0.University0.edu>" 4804
+// p_var_3 = "ub:subOrganizationOf" 5
+// p_var_4 = "ub:Research-Group" 22818
+// p_var_5 = "a" 14
+  search(&TheGraph, var_2, p_var_3, p_var_4, p_var_5);
+  printf("%d\n", numAnswers);
+  return numAnswers;
+}
+
+
+#ifdef DEBUG
+int main() {
+  return test(8204, 14, 15362, 10) != 10;
+  //return test(4804, 5, 22818, 14);
+}
+#endif
+
