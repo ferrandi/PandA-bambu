0497d16e11d94d5118833791dff9b99ef74d4c1e
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2018-02-16 08:56:38 +0100

    Fixed clang regression scripts.

--- panda_regressions/hls/clang4_regression_simple_ext_pipelined.sh
+++ panda_regressions/hls/clang4_regression_simple_ext_pipelined.sh
@@ -4,5 +4,5 @@ $(dirname $0)/../../etc/scripts/test_panda.py gcc_regression_simple --tool=bambu
    --args="--configuration-name=CLANG4_O1 -O1 -lm --simulate --experimental-setup=BAMBU --compiler=I386_CLANG4 --channels-type=MEM_ACC_NN" \
    --args="--configuration-name=CLANG4_O2 -O2 -lm --simulate --experimental-setup=BAMBU --compiler=I386_CLANG4 --channels-type=MEM_ACC_NN" \
    --args="--configuration-name=CLANG4_O3 -O3 -lm --simulate --experimental-setup=BAMBU --compiler=I386_CLANG4 --channels-type=MEM_ACC_NN" \
-   -o output_clang40_simple_ext_pipelined -b$(dirname $0) --table=output_clang4_simple_ext_pipelined.tex --name="Clang4RegressionSimple-ExtPipelined" $@
+   -o output_clang4_simple_ext_pipelined -b$(dirname $0) --table=output_clang4_simple_ext_pipelined.tex --name="Clang4RegressionSimple-ExtPipelined" $@
 exit $?
