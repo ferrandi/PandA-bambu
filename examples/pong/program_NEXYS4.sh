ec9d7806423d0e9e311f81bee1122740ce40b69c
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2018-02-26 18:54:32 +0100

    Updated pong and breakout scripts.

--- examples/pong/program_NEXYS4.sh
+++ examples/pong/program_NEXYS4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-. /opt/Xilinx/Vivado/2016.4/settings64.sh >& /dev/null; 
+. /opt/Xilinx/Vivado/2017.4/settings64.sh >& /dev/null; 
 
 vivado -mode batch -nojournal -nolog -source program_nexys4.tcl
 
