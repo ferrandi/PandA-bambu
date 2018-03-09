6cea99bc8e0d66fbeae443bfca324157a474d6be
ferrandi <fabrizio.ferrandi@polimi.it>
2018-03-09 12:27:10 +0100

    Changed arf test script.

--- examples/arf/bambu.sh
+++ examples/arf/bambu.sh
@@ -5,7 +5,7 @@ export PATH=../../src:../../../src:/opt/panda/bin:$PATH
 
 mkdir -p hls
 cd hls
-echo "#synthesis"
+echo "#synthesis script generation"
 bambu -v2 --print-dot $root_dir/module.c --no-iob
 return_value=$?
 if test $return_value != 0; then
@@ -15,8 +15,8 @@ cd ..
 
 mkdir -p testbench
 cd testbench
-echo "#synthesis and generation of testbench with ICARUS"
-bambu -v5 --print-dot $root_dir/module.c --generate-tb=$root_dir/test.xml --simulator=ICARUS --no-iob
+echo "#synthesis script generation and testbench simulation with VERILATOR"
+bambu -v5 --print-dot $root_dir/module.c --generate-tb=$root_dir/test.xml --simulator=VERILATOR --no-iob --simulate
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
