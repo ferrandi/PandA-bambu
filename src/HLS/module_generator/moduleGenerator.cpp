d27e84eddfabf19380373f14dee912a12d959872
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2018-07-03 10:18:50 +0200

    Fixed some typos.

--- src/HLS/module_generator/moduleGenerator.cpp
+++ src/HLS/module_generator/moduleGenerator.cpp
@@ -214,7 +214,7 @@ std::string moduleGenerator::GenerateHDL(const std::string& hdl_template, std::v
    cpp_code_body += "std::string size_bus_bitsize = \"" + STR(HLSMgr->Rmem->get_bus_size_bitsize()) + "\";\n";
    cpp_code_body += "std::string _specializing_string = \"" + specializing_string + "\";\n";
    if(parameters->isOption(OPT_channels_number) && parameters->getOption<unsigned int>(OPT_channels_number) > 1)
-      cpp_code_body += "unsigned int _number_of_channes = " + STR(parameters->getOption<unsigned int>(OPT_channels_number)) + ";\n";
+      cpp_code_body += "unsigned int _number_of_channels = " + STR(parameters->getOption<unsigned int>(OPT_channels_number)) + ";\n";
 
    cpp_code_body += "\n\n\n";
 
