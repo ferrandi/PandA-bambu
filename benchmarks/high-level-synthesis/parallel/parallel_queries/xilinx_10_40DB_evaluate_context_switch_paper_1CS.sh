06dea155598626fa29424f0135827a6d0fafcff2
Marco Lattuada <marco.lattuada@polimi.it>
2018-07-26 12:39:32 +0200

    Fixed scripts

--- benchmarks/high-level-synthesis/parallel/parallel_queries/xilinx_10_40DB_evaluate_context_switch_paper_1CS.sh
+++ benchmarks/high-level-synthesis/parallel/parallel_queries/xilinx_10_40DB_evaluate_context_switch_paper_1CS.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-COMMONARGS="--compiler=I386_GCC49 --std=c99 --experimental-set=BAMBU -O3 -v3 -fno-delete-null-pointer-checks -fopenmp --pragma-parse --mem-delay-read=20 --mem-delay-write=20 --channels-type=MEM_ACC_11 --memory-allocation-policy=NO_BRAM --no-iob --device-name=xc7vx690t-3ffg1930-VVD --simulate --clock-period=10  -DMAX_VERTEX_NUMBER=1309073 -DMAX_EDGE_NUMBER=5309056 --max-sim-cycles=2000000000"
+COMMONARGS="--compiler=I386_GCC49 --std=c99 --experimental-set=BAMBU -O3 -v3 -fno-delete-null-pointer-checks -fopenmp --pragma-parse --mem-delay-read=20 --mem-delay-write=20 --channels-type=MEM_ACC_11 --memory-allocation-policy=NO_BRAM --no-iob --device-name=xc7vx690t-3ffg1930-VVD --evaluation --clock-period=10  -DMAX_VERTEX_NUMBER=1309073 -DMAX_EDGE_NUMBER=5309056 --max-sim-cycles=2000000000"
  NAME=`basename $0 .sh`
 `dirname $0`/../../../../etc/scripts/test_panda.py --spider-style="../lib/latex_format_bambu_results_xilinx.xml" --tool=bambu -l`dirname $0`/list_40DB -ooutput_$NAME --commonargs="$COMMONARGS" --ulimit="-f 4097152 -v 8388608 -s 16384"\
+      --args="--configuration-name=01W-02CH-1C-1CS -DN_THREADS=1  --num-threads=1  --memory-banks-number=2  --channels-number=1 --context_switch=1"\
+      --args="--configuration-name=01W-04CH-2C-1CS -DN_THREADS=1  --num-threads=1  --memory-banks-number=4  --channels-number=2 --context_switch=1"\
+      --args="--configuration-name=01W-08CH-2C-1CS -DN_THREADS=1  --num-threads=1  --memory-banks-number=8  --channels-number=2 --context_switch=1"\
+      --args="--configuration-name=01W-16CH-2C-1CS -DN_THREADS=1  --num-threads=1  --memory-banks-number=16 --channels-number=2 --context_switch=1"\
       --args="--configuration-name=02W-02CH-1C-1CS -DN_THREADS=2  --num-threads=2  --memory-banks-number=2  --channels-number=1 --context_switch=1"\
       --args="--configuration-name=02W-04CH-2C-1CS -DN_THREADS=2  --num-threads=2  --memory-banks-number=4  --channels-number=2 --context_switch=1"\
       --args="--configuration-name=02W-08CH-2C-1CS -DN_THREADS=2  --num-threads=2  --memory-banks-number=8  --channels-number=2 --context_switch=1"\
