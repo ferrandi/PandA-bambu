eda7400d6531564155cf52b2bf9c12f78cb0fb6b
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2018-08-09 17:53:14 +0200

    Fixed indentation.

--- etc/libbambu/libm/s_scalbn.c
+++ etc/libbambu/libm/s_scalbn.c
@@ -28,27 +28,27 @@
 double scalbn (double x, int n)
 {
 	int  k,hx,lx;
-	hx = GET_HI(x);
-	lx = GET_LO(x);
-        k = (hx&0x7ff00000)>>20;		/* extract exponent */
-        if (k==0) {				/* 0 or subnormal x */
-            if ((lx|(hx&0x7fffffff))==0) return x; /* +-0 */
-	    x *= two54; 
-	    hx = GET_HI(x);
-	    k = ((hx&0x7ff00000)>>20) - 54; 
-            if (n< -50000) return tiny*x; 	/*underflow*/
-	    }
-        if (k==0x7ff) return x;		/* NaN or Inf */
-        k = k+n; 
-        if (k >  0x7fe) return huge*copysign(huge,x); /* overflow  */
-        if (k > 0) 				/* normal result */
-	    {SET_HIGH_WORD(x, (hx&0x800fffff)|(k<<20)); return x;}
-        if (k <= -54)
-            if (n > 50000) 	/* in case integer overflow in n+k */
-        return huge*copysign(huge,x);	/*overflow*/
-        else return tiny*copysign(tiny,x); 	/*underflow*/
-        k += 54;				/* subnormal result */
-        SET_HIGH_WORD(x, (hx&0x800fffff)|(k<<20));
-        return x*twom54;
+    hx = GET_HI(x);
+    lx = GET_LO(x);
+    k = (hx&0x7ff00000)>>20;		/* extract exponent */
+    if (k==0) {				/* 0 or subnormal x */
+       if ((lx|(hx&0x7fffffff))==0) return x; /* +-0 */
+       x *= two54;
+       hx = GET_HI(x);
+       k = ((hx&0x7ff00000)>>20) - 54;
+       if (n< -50000) return tiny*x; 	/*underflow*/
+    }
+    if (k==0x7ff) return x;		/* NaN or Inf */
+    k = k+n;
+    if (k >  0x7fe) return huge*copysign(huge,x); /* overflow  */
+    if (k > 0) 				/* normal result */
+    {SET_HIGH_WORD(x, (hx&0x800fffff)|(k<<20)); return x;}
+    if (k <= -54)
+       if (n > 50000) 	/* in case integer overflow in n+k */
+          return huge*copysign(huge,x);	/*overflow*/
+       else return tiny*copysign(tiny,x); 	/*underflow*/
+    k += 54;				/* subnormal result */
+    SET_HIGH_WORD(x, (hx&0x800fffff)|(k<<20));
+    return x*twom54;
 }
 
