58e64425a41f97bee7341eb1ad0e20e9a127cc3d
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2018-08-24 10:53:16 +0200

    Removed too memory consuming tests.

--- panda_regressions/hls/discrepancy1.sh
+++ panda_regressions/hls/discrepancy1.sh
@@ -3,6 +3,6 @@ abs_script=$(readlink -e $0)
 dir_script=$(dirname $abs_script)
 $dir_script/../../etc/scripts/test_panda.py --tool=bambu \
    --args="--configuration-name=GCC7_O2 -O2 -lm --simulate --experimental-setup=BAMBU --compiler=I386_GCC7 --discrepancy" \
-   -ldiscrepancy_list -o output_discrepancy_test1 --ulimit="-f 5100000 -v 6000000 -s 16384" -j6 \
+   -ldiscrepancy_list -o output_discrepancy_test1 --ulimit="-f 5000000 -v 5000000 -s 16384" -j7 \
    -b$dir_script --table=output_discrepancy1.tex --name="Discrepancy" $@
 exit $?
