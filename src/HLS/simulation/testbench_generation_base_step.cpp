53c265587ad26de6ab4e672110000bc2c3aebcb9
Pietro Fezzardi <pietrofezzardi@gmail.com>
2018-12-18 18:21:56 +0100

    [TestbenchGenerationBaseStep] now depends on HWDiscrepancyAnalysis if --discrepancy-hw is passed

--- src/HLS/simulation/testbench_generation_base_step.cpp
+++ src/HLS/simulation/testbench_generation_base_step.cpp
@@ -121,6 +121,10 @@ const std::unordered_set<std::tuple<HLSFlowStep_Type, HLSFlowStepSpecializationC
       case DEPENDENCE_RELATIONSHIP:
       {
          ret.insert(std::make_tuple(HLSFlowStep_Type::C_TESTBENCH_EXECUTION, HLSFlowStepSpecializationConstRef(), HLSFlowStep_Relationship::TOP_FUNCTION));
+         if(parameters->isOption(OPT_discrepancy_hw) and parameters->getOption<bool>(OPT_discrepancy_hw))
+         {
+            ret.insert(std::make_tuple(HLSFlowStep_Type::HW_DISCREPANCY_ANALYSIS, HLSFlowStepSpecializationConstRef(), HLSFlowStep_Relationship::TOP_FUNCTION));
+         }
          break;
       }
       case INVALIDATION_RELATIONSHIP:
