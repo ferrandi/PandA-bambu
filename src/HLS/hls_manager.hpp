01920db35878a7b372b3d78556bbb6dede9174e0
Pietro Fezzardi <pietrofezzardi@gmail.com>
2018-12-19 09:08:09 +0100

    [HLS_manager] remove old unused member

--- src/HLS/hls_manager.hpp
+++ src/HLS/hls_manager.hpp
@@ -102,12 +102,6 @@ class HLS_manager : public application_manager
    /// The HDL files
    std::list<std::string> hdl_files;
 
-   /**
-    * A map to store the vcd signals to be dumped. The key is the scope, and
-    * the mapped set contains all the signals to be dumped for that scope
-    */
-   std::map<std::string, std::set<std::string>> selected_vcd_signals;
-
 #if HAVE_TASTE
    /// The information collected from aadl files
    const AadlInformationRef aadl_information;
