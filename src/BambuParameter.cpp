1210b8a4ca2019befbebaecc327faa363862aa67
Pietro Fezzardi <pietrofezzardi@gmail.com>
2018-12-20 14:38:47 +0100

    [BambuParameter] check that --discrepancy-hw is used only with verilog

--- src/BambuParameter.cpp
+++ src/BambuParameter.cpp
@@ -3070,6 +3070,10 @@ void BambuParameter::CheckParameters()
    {
       THROW_ERROR("--discrepancy-hw is only compatible with classic FSM controllers");
    }
+   if(isOption(OPT_discrepancy_hw) and getOption<bool>(OPT_discrepancy_hw) and static_cast<HDLWriter_Language>(getOption<unsigned int>(OPT_writer_language)) != HDLWriter_Language::VERILOG)
+   {
+      THROW_ERROR("--discrepancy-hw is only compatible with Verilog");
+   }
    if(isOption(OPT_discrepancy) and getOption<bool>(OPT_discrepancy))
    {
       if(false
