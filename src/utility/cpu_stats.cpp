bd57a9a81c915e05543c7c8faa5a8febda501077
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2018-12-26 19:49:12 +0100

    fixed a compilation error.

--- src/utility/cpu_stats.cpp
+++ src/utility/cpu_stats.cpp
@@ -58,7 +58,7 @@ std::string PrintVirtualDataMemoryUsage()
 {
 #ifdef _WIN32
    return STR("unavailable");
-#elif __APPLE__
+#elif defined(__APPLE__)
    return STR("unavailable");
 #else
    extern int end, etext, edata;
@@ -144,7 +144,7 @@ void util_print_cpu_stats(std::ostream& os)
    else
       os << vm_limit << "\n";
    os << "Page faults       = " << page_faults << "\n";
-#elif __APPLE__
+#elif defined(__APPLE__)
    ;// do nothing
 #else
    extern int end, etext, edata;
