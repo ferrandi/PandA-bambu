6a31eb07dd5958c91d51d7daea72b51f6c3374ac
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-01-25 10:38:21 +0100

    Removed a dependency.

--- src/HLS/module_allocation/hls_bit_value.cpp
+++ src/HLS/module_allocation/hls_bit_value.cpp
@@ -125,7 +125,6 @@ const std::unordered_set<std::tuple<HLSFlowStep_Type, HLSFlowStepSpecializationC
    {
       case DEPENDENCE_RELATIONSHIP:
       {
-         ret.insert(std::make_tuple(HLSFlowStep_Type::HLS_BIT_VALUE, HLSFlowStepSpecializationConstRef(), HLSFlowStep_Relationship::CALLED_FUNCTIONS));
          ret.insert(std::make_tuple(HLSFlowStep_Type::INITIALIZE_HLS, HLSFlowStepSpecializationConstRef(), HLSFlowStep_Relationship::SAME_FUNCTION));
          ret.insert(std::make_tuple(parameters->getOption<HLSFlowStep_Type>(OPT_memory_allocation_algorithm), HLSFlowStepSpecializationConstRef(), HLSFlowStep_Relationship::WHOLE_APPLICATION));
          break;
