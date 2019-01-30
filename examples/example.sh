77a1663d6c83f5e0a8640fecf0a9334f68c5af60
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-01-30 11:54:02 +0100

    Temporary changes to test Jenkins script.

--- examples/example.sh
+++ examples/example.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 abs_script=$(readlink -e $0)
 dir_script=$(dirname $abs_script)
-$dir_script/softfloat/all.sh $@
-return_value=$?
-if test $return_value != 0; then
-   exit $return_value
-fi
-$dir_script/libm/all.sh $@
-return_value=$?
-if test $return_value != 0; then
-   exit $return_value
-fi
-$dir_script/CHStone/all.sh $@
-return_value=$?
-if test $return_value != 0; then
-   exit $return_value
-fi
-$dir_script/MachSuite/all.sh $@
-return_value=$?
-if test $return_value != 0; then
-   exit $return_value
-fi
-$dir_script/hls_study/all.sh $@
-return_value=$?
-if test $return_value != 0; then
-   exit $return_value
-fi
-$dir_script/omp_simd/5SGXEA7N2F45C1_10.0_O2.sh  $@
-return_value=$?
-if test $return_value != 0; then
-   exit $return_value
-fi
+#$dir_script/softfloat/all.sh $@
+#return_value=$?
+#if test $return_value != 0; then
+#   exit $return_value
+#fi
+#$dir_script/libm/all.sh $@
+#return_value=$?
+#if test $return_value != 0; then
+#   exit $return_value
+#fi
+#$dir_script/CHStone/all.sh $@
+#return_value=$?
+#if test $return_value != 0; then
+#   exit $return_value
+#fi
+#$dir_script/MachSuite/all.sh $@
+#return_value=$?
+#if test $return_value != 0; then
+#   exit $return_value
+#fi
+#$dir_script/hls_study/all.sh $@
+#return_value=$?
+#if test $return_value != 0; then
+#   exit $return_value
+#fi
+#$dir_script/omp_simd/5SGXEA7N2F45C1_10.0_O2.sh  $@
+#return_value=$?
+#if test $return_value != 0; then
+#   exit $return_value
+#fi
 $dir_script/omp_simd/xc7vx690t-3ffg1930-VVD_10.0_O2.sh  $@
 return_value=$?
 if test $return_value != 0; then
