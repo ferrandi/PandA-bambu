3e243b3716669e15f1223dbf5aeaae0656f488c1
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-01-30 11:00:21 +0100

    Hopefully fixed an infinite loop.

--- src/HLS/memory/memory_allocation.cpp
+++ src/HLS/memory/memory_allocation.cpp
@@ -537,15 +537,15 @@ bool memory_allocation::HasToBeExecuted() const
    if(memory_version == 0 or memory_version != HLSMgr->GetMemVersion())
       return true;
    std::map<unsigned int, unsigned int> cur_bb_ver;
-   std::map<unsigned int, unsigned int> cur_bitvalue_ver;
+   //std::map<unsigned int, unsigned int> cur_bitvalue_ver;
    const CallGraphManagerConstRef CGMan = HLSMgr->CGetCallGraphManager();
    for(const auto i : CGMan->GetReachedBodyFunctions())
    {
       const FunctionBehaviorConstRef FB = HLSMgr->CGetFunctionBehavior(i);
       cur_bb_ver[i] = FB->GetBBVersion();
-      cur_bitvalue_ver[i] = FB->GetBitValueVersion();
+      //cur_bitvalue_ver[i] = FB->GetBitValueVersion();
    }
-   return cur_bb_ver != last_bb_ver || cur_bitvalue_ver != last_bitvalue_ver;
+   return cur_bb_ver != last_bb_ver /*|| cur_bitvalue_ver != last_bitvalue_ver*/;
 }
 
 DesignFlowStep_Status memory_allocation::Exec()
