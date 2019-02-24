04a108a2fd3193583fd9806f77629015603dc5ff
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-02-24 14:54:37 +0100

    Removed parallelism restriction on discrepancy regression scripts.

--- panda_regressions/hls/discrepancy.sh
+++ panda_regressions/hls/discrepancy.sh
@@ -6,6 +6,6 @@ $dir_script/../../etc/scripts/test_panda.py --tool=bambu \
    --args="--configuration-name=GCC7_O1 -O1 -lm --simulate --experimental-setup=BAMBU --compiler=I386_GCC7 --discrepancy" \
    --args="--configuration-name=GCC7_O2 -O2 -lm --simulate --experimental-setup=BAMBU --compiler=I386_GCC7 --discrepancy" \
    --args="--configuration-name=GCC7_O3 -O3 -lm --simulate --experimental-setup=BAMBU --compiler=I386_GCC7 --discrepancy" \
-   -ldiscrepancy_list -o output_discrepancy_test -j3 \
+   -ldiscrepancy_list -o output_discrepancy_test \
    -b$dir_script --table=output_discrepancy.tex --name="Discrepancy" $@
 exit $?
