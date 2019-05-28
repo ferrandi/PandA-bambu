aa92ae78887bf7af76a7fe2a5f49f3e8e81fe6d1
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-05-28 16:01:56 +0200

    Added parm2ssa data structure to improve memory allocation.

--- src/behavior/application_manager.hpp
+++ src/behavior/application_manager.hpp
@@ -119,6 +119,9 @@ class application_manager
    /// debugging level of the class
    const int debug_level;
 
+   /// put into relation formal parameters and the associated ssa variables
+   CustomMap<unsigned,unsigned> Parm2SSA_map;
+
    /**
     * Returns the values produced by a vertex (recursive version)
     */
@@ -309,6 +312,22 @@ class application_manager
     */
    void RegisterTransformation(const std::string& step, const tree_nodeConstRef new_tn);
 #endif
+
+   /**
+    * \brief getSSAFromParm returns the ssa_name index associated with the parm_decl index, 0 in case there is not an associated index
+    * \param parm_index is the parm_decl index for which we look for the associated ssa_name index
+    */
+   unsigned getSSAFromParm(unsigned parm_index) const;
+   /**
+    * @brief setSSAFromParm defines the parm_decl versus ssa_name relation
+    * @param parm_index is the index of the parm_decl
+    * @param ssa_index is the index of the ssa_name
+    */
+   void setSSAFromParm(unsigned int parm_index, unsigned ssa_index);
+   /**
+    * @brief clearParm2SSA cleans the map putting into relation parm_decl and ssa_name
+    */
+   void clearParm2SSA();
 };
 /// refcount definition of the class
 typedef refcount<application_manager> application_managerRef;
