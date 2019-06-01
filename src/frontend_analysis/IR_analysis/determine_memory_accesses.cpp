0b292a2c457a65028429c4e4da1e6c88dc7e5532
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-06-01 14:51:50 +0200

    Fixed an issue in the parm2ssa step dependency.

--- src/frontend_analysis/IR_analysis/determine_memory_accesses.cpp
+++ src/frontend_analysis/IR_analysis/determine_memory_accesses.cpp
@@ -633,6 +633,9 @@ void determine_memory_accesses::analyze_node(unsigned int node_id, bool left_p, 
                      }
                   }
                }
+               else if(!AppM->isParmUsed(formal_par_index))
+                  INDENT_DBG_MEX(DEBUG_LEVEL_VERY_PEDANTIC, debug_level, "---Parameter is not used in the function body.");
+
                /// check if the formal parameter has been allocated in memory.
                if(FBcalled->is_variable_mem(formal_par_index))
                {
@@ -842,6 +845,8 @@ void determine_memory_accesses::analyze_node(unsigned int node_id, bool left_p, 
                      }
                   }
                }
+               else if(!AppM->isParmUsed(formal_par_index))
+                  INDENT_DBG_MEX(DEBUG_LEVEL_VERY_PEDANTIC, debug_level, "---Parameter is not used in the function body.");
                /// check if the formal parameter has been allocated in memory.
                if(FBcalled->is_variable_mem(formal_par_index))
                {
