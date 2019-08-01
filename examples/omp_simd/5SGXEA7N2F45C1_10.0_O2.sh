663e0679ea66921f342ccfdc0490df2c56df8f29
Marco Lattuada <marco.lattuada@polimi.it>
2019-08-01 15:21:04 +0200

    Reverted gcc to 4.9 in simd tests

--- examples/omp_simd/5SGXEA7N2F45C1_10.0_O2.sh
+++ examples/omp_simd/5SGXEA7N2F45C1_10.0_O2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ARGS="--compiler=I386_GCC6 --device-name=5SGXEA7N2F45C1 --evaluation -fwhole-program -fno-delete-null-pointer-checks --clock-period=10 --experimental-setup=BAMBU-BALANCED-MP --no-iob"
+ARGS="--compiler=I386_GCC49 --device-name=5SGXEA7N2F45C1 --evaluation -fwhole-program -fno-delete-null-pointer-checks --clock-period=10 --experimental-setup=BAMBU-BALANCED-MP --no-iob"
 script=$(readlink -e $0)
 root_dir=$(dirname $script)
 NAME=$(basename $0 .sh)
