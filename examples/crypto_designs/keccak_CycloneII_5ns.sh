895c6ec4aaf41cd7d3737a03f4ccdff401271a19
Marco Lattuada <marco.lattuada@polimi.it>
2019-08-17 13:57:09 +0200

    Updated crypto_designs scripts

--- examples/crypto_designs/keccak_CycloneII_5ns.sh
+++ examples/crypto_designs/keccak_CycloneII_5ns.sh
@@ -7,5 +7,9 @@ rm -rf keccak_CycloneII_4ns
 mkdir keccak_CycloneII_4ns
 cd keccak_CycloneII_4ns
 bambu -O3 --pretty-print=a.c --top-fname=kekka_coproc -v4 --addr-bus-bitsize=9 --base-address=256 --sparse-memory=off --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_11 -finline-limit=10000 --clock-period=4 --device-name=EP2C70F896C6-R $root_dir/Keccak.c --generate-tb=$root_dir/test.xml  --experimental-setup=BAMBU --evaluation -fno-ivopts
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
 cd ..
 exit 0
