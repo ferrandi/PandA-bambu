5dee7a165adc0c58e04178a3d94ac30f6018e8f9
Serena Curzel <serena.curzel@mail.polimi.it>
2019-09-16 20:55:18 +0200

    conv2d second example

---
+++ examples/onnx/11_conv2d_b/bambu.sequential.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+script=$(readlink -e $0)
+root_dir=$(dirname $script)
+
+export PATH=../../../../../panda/bin:../../src:../../../src:/opt/panda/bin:$PATH
+rm -rf bambu
+mkdir bambu
+cd bambu
+bambu $root_dir/11_conv2d_b.parallel.c \
+      -I $root_dir/../common/ \
+      -DBAMBU_PROFILING\
+      -fno-inline -fno-inline-functions\
+      -v4 --compiler=I386_GCC49 --print-dot \
+      --top-fname=fused_conv2d_wrapper --top-rtldesign-name=conv \
+      --channels-type=MEM_ACC_11 \
+      --memory-allocation-policy=NO_BRAM \
+      --mem-delay-read=20 --mem-delay-write=20 --simulator=VERILATOR \
+      --generate-tb=$root_dir/test.xml \
+      --pretty-print=a.c --no-iob --device-name=xc7vx690t-3ffg1930-VVD --clock-period=3.3 --evaluation 
