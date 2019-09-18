283bb75d54a0c2092384c66f4e8245e2c8f1f8c4
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-09-18 16:06:04 +0200

    Improved register_file description.

--- examples/onnx/02_vecmul_b/bambu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./scalarize.sh
-export PATH=../../src:../../../src:/opt/panda/bin:$PATH
-rm -rf bambu
-mkdir bambu
-cd bambu
-bambu ../02_vecmul_b.scalarized.ll -v4 --compiler=I386_CLANG6 --print-dot --top-fname=fused_multiply --do-not-expose-globals --pretty-print=a.c --no-iob --device-name=EP4SGX530KH40C2 --evaluation=PERIOD,AREA,FREQUENCY,CLOCK_SLACK,REGISTERS,DSPS,BRAMS
