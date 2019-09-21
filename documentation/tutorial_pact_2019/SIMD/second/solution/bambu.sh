ab1288543afd63f6acfd0df5e19eca3820fde20b
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-09-21 14:11:42 +0200

    Fixed some pact19 scripts.

--- documentation/tutorial_pact_2019/SIMD/second/solution/bambu.sh
+++ documentation/tutorial_pact_2019/SIMD/second/solution/bambu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script=$(readlink -e $0)
 root_dir=$(dirname $script)/..
-bambu --compiler=I386_GCC49 --device-name=5SGXEA7N2F45C1 --simulate -fwhole-program -fno-delete-null-pointer-checks --clock-period=10 --experimental-setup=BAMBU-BALANCED-MP -fdisable-tree-cunroll -fdisable-tree-ivopts --param max-inline-insns-auto=1000  ${root_dir}/histogram.c -fopenmp-simd=4
+bambu --device-name=5SGXEA7N2F45C1 --simulate -fwhole-program -fno-delete-null-pointer-checks --clock-period=10 --experimental-setup=BAMBU-BALANCED-MP -fdisable-tree-cunroll -fdisable-tree-ivopts --param max-inline-insns-auto=1000  ${root_dir}/histogram.c -fopenmp-simd=4
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
