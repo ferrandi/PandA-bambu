1fcbcb5681916f99b8f6f96b400a7cbcee68ed35
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-09-25 18:51:54 +0200

    Fixed some compilation issues.

--- ext/mockturtle/include/mockturtle/algorithms/collapse_mapped.hpp
+++ ext/mockturtle/include/mockturtle/algorithms/collapse_mapped.hpp
@@ -194,7 +194,7 @@ public:
     } );
 
     /* outputs */
-    ntk.foreach_po( [&]( auto const& f ) {
+    ntk.foreach_po( [&]( auto const& f, auto index ) {
       if ( ntk.is_complemented( f ) && node_driver_type[f] == driver_type::mixed )
       {
         dest.create_po( opposites[ntk.get_node( f )] );
