b16d97ddde96f7a06ef8e66f3d9ae7516fd7edaa
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-10-06 17:22:03 +0200

    Fixed a possible problem with create_lut.

--- src/frontend_analysis/IR_analysis/lut_transformation.cpp
+++ src/frontend_analysis/IR_analysis/lut_transformation.cpp
@@ -361,7 +361,7 @@ class klut_network_ext : public mockturtle::klut_network
          while((res0.size() << 2) < tt.num_bits())
             res0 = "0" + res0;
       }
-      while((res0.size() << 2) > tt.num_bits())
+      while((res0.size() << 2) > tt.num_bits() && tt.num_vars() > 1)
          res0 = res0.substr(1);
 
       kitty::create_from_hex_string(tt, res0);
