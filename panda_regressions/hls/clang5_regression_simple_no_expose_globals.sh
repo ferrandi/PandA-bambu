4cc98f44415b054517c6456608b592952a80d095
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2019-10-08 17:23:18 +0200

    Fixed regression scripts.

--- panda_regressions/hls/clang5_regression_simple_no_expose_globals.sh
+++ panda_regressions/hls/clang5_regression_simple_no_expose_globals.sh
@@ -4,5 +4,5 @@ $(dirname $0)/../../etc/scripts/test_panda.py gcc_regression_simple --tool=bambu
    --args="--configuration-name=CLANG5_O1 -O1 -lm --simulate --do-not-expose-globals --experimental-setup=BAMBU --compiler=I386_CLANG5" \
    --args="--configuration-name=CLANG5_O2 -O2 -lm --simulate --do-not-expose-globals --experimental-setup=BAMBU --compiler=I386_CLANG5" \
    --args="--configuration-name=CLANG5_O3 -O3 -lm --simulate --do-not-expose-globals --experimental-setup=BAMBU --compiler=I386_CLANG5" \
-   -o output_clang5_simple_do_not_expose_globals -b$(dirname $0) --table=output_clang5_simple.tex --name="Clang5RegressionSimple" $@
+   -o output_clang5_simple_do_not_expose_globals -b$(dirname $0) --table=output_clang5_simple_do_not_expose_globals.tex --name="Clang5RegressionSimpleDNEG" $@
 exit $?
--- panda_regressions/hls/clang7_regression_simple_no_expose_globals.sh
+++ panda_regressions/hls/clang7_regression_simple_no_expose_globals.sh
@@ -4,5 +4,5 @@ $(dirname $0)/../../etc/scripts/test_panda.py gcc_regression_simple --tool=bambu
    --args="--configuration-name=CLANG7_O1 -O1 -lm --simulate --do-not-expose-globals --experimental-setup=BAMBU --compiler=I386_CLANG7" \
    --args="--configuration-name=CLANG7_O2 -O2 -lm --simulate --do-not-expose-globals --experimental-setup=BAMBU --compiler=I386_CLANG7" \
    --args="--configuration-name=CLANG7_O3 -O3 -lm --simulate --do-not-expose-globals --experimental-setup=BAMBU --compiler=I386_CLANG7" \
-   -o output_clang7_simple_do_not_expose_globals -b$(dirname $0) --table=output_clang7_simple.tex --name="Clang7RegressionSimple" $@
+   -o output_clang7_simple_do_not_expose_globals -b$(dirname $0) --table=output_clang7_simple_do_not_expose_globals.tex --name="Clang7RegressionSimpleDNEG" $@
 exit $?
