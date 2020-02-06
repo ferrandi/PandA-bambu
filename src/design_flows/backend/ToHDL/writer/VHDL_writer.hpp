17bb8539ef6919a8ffc009164c93082409d50d3f
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2020-02-06 17:14:50 +0100

    Various improvement about yosys support.

--- src/design_flows/backend/ToHDL/writer/VHDL_writer.hpp
+++ src/design_flows/backend/ToHDL/writer/VHDL_writer.hpp
@@ -209,7 +209,7 @@ struct VHDL_writer : public language_writer
     * @param n_states is the number of states.
     */
    void write_transition_output_functions(bool single_proc, unsigned int output_index, const structural_objectRef& cir, const std::string& reset_state, const std::string& reset_port, const std::string& start_port, const std::string& clock_port,
-                                          std::vector<std::string>::const_iterator& first, std::vector<std::string>::const_iterator& end) override;
+                                          std::vector<std::string>::const_iterator& first, std::vector<std::string>::const_iterator& end, bool) override;
 
    /**
     * Write in the proper language the behavioral description of the module described in "Not Parsed" form.
