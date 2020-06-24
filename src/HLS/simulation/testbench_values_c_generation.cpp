cc69306fa1fc16cb2770504aa4131cc5024bb7bd
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2020-06-24 14:28:09 +0200

    Removed unused includes.

--- src/HLS/simulation/testbench_values_c_generation.cpp
+++ src/HLS/simulation/testbench_values_c_generation.cpp
@@ -46,14 +46,6 @@
 /// Header include
 #include "testbench_values_c_generation.hpp"
 
-/// Autoheader includes
-#include "config_HAVE_I386_GCC47_COMPILER.hpp"
-#include "config_HAVE_I386_GCC48_COMPILER.hpp"
-#include "config_HAVE_I386_GCC49_COMPILER.hpp"
-#include "config_HAVE_I386_GCC5_COMPILER.hpp"
-#include "config_HAVE_I386_GCC6_COMPILER.hpp"
-#include "config_HAVE_I386_GCC7_COMPILER.hpp"
-
 ///. include
 #include "Parameter.hpp"
 
