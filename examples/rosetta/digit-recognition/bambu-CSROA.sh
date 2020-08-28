e00eb094e8f5c5cbab4ebb043570a98fadf4ec01
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2020-08-28 15:26:39 +0200

    Slightly changed TCAD_SROA demo scripts.

--- examples/rosetta/digit-recognition/bambu-CSROA.sh
+++ examples/rosetta/digit-recognition/bambu-CSROA.sh
@@ -5,7 +5,7 @@ export PATH=../../src:../../../src:/opt/panda/bin:$PATH
 
 mkdir -p bambu_CSROA
 cd bambu_CSROA
-bambu --simulate --simulator=VERILATOR $root_dir/digitrec_sw.c -v4 --top-fname=DigitRec_sw -s --do-not-expose-globals --generate-tb=$root_dir/test.xml --std=c99 --compiler=I386_CLANG4 --panda-parameter=enable-CSROA=1 
+bambu --generate-interface=INFER --simulate --simulator=VERILATOR $root_dir/digitrec_sw.c -v4 --top-fname=DigitRec_sw -s --do-not-expose-globals --generate-tb=$root_dir/test.xml --std=c99 --compiler=I386_CLANG4 --panda-parameter=enable-CSROA=1 
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
--- examples/rosetta/digit-recognition/bambu.sh
+++ examples/rosetta/digit-recognition/bambu.sh
@@ -5,7 +5,7 @@ export PATH=../../src:../../../src:/opt/panda/bin:$PATH
 
 mkdir -p bambu
 cd bambu
-bambu --simulate --simulator=VERILATOR $root_dir/digitrec_sw.c -v4 --top-fname=DigitRec_sw -s --do-not-expose-globals --generate-tb=$root_dir/test.xml --std=c99 --compiler=I386_CLANG4 --panda-parameter=enable-CSROA=0 
+bambu --generate-interface=INFER --simulate --simulator=VERILATOR $root_dir/digitrec_sw.c -v4 --top-fname=DigitRec_sw -s --do-not-expose-globals --generate-tb=$root_dir/test.xml --std=c99 --compiler=I386_CLANG4 --panda-parameter=enable-CSROA=0 
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
