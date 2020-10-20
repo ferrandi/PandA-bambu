9c6c544874277af60f8c47aa223f632be18e2007
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2020-10-20 14:11:02 +0200

    Improved crc scripts.

--- examples/crc/bambu_altera.sh
+++ examples/crc/bambu_altera.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+script=$(readlink -e $0)
+root_dir=$(dirname $script)
 export PATH=/opt/panda/bin:$PATH
 
 mkdir -p icrc
 cd icrc
 echo "#synthesis of icrc"
-timeout 2h bambu ../spec.c --top-fname=icrc --simulator=VERILATOR --device-name=EP2C70F896C6-R --simulate --generate-tb=../test_icrc.xml --experimental-setup=BAMBU -v2
+timeout 2h bambu $root_dir/spec.c --top-fname=icrc --simulator=VERILATOR --device-name=EP2C70F896C6-R --simulate --generate-tb=$root_dir/test_icrc.xml --experimental-setup=BAMBU -v2
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
@@ -14,7 +16,7 @@ cd ..
 mkdir -p main
 cd main
 echo "#synthesis of main"
-timeout 2h bambu ../spec.c  --simulator=VERILATOR  --device-name=EP2C70F896C6-R --simulate --generate-tb=../test.xml --experimental-setup=BAMBU -v2
+timeout 2h bambu $root_dir/spec.c  --simulator=VERILATOR  --device-name=EP2C70F896C6-R --simulate --generate-tb=$root_dir/test.xml --experimental-setup=BAMBU -v2
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
--- examples/crc/bambu_yosys.sh
+++ examples/crc/bambu_yosys.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+script=$(readlink -e $0)
+root_dir=$(dirname $script)
 export PATH=/opt/panda/bin:$PATH
 
 mkdir -p icrc
 cd icrc
 echo "#synthesis of icrc"
-timeout 2h bambu ../spec.c --top-fname=icrc --simulator=VERILATOR --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=../test_icrc.xml --experimental-setup=BAMBU --no-iob -v2 
+timeout 2h bambu $root_dir/spec.c --top-fname=icrc --simulator=VERILATOR --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=$root_dir/test_icrc.xml --experimental-setup=BAMBU --no-iob -v2 
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
@@ -14,7 +16,7 @@ cd ..
 mkdir -p main
 cd main
 echo "#synthesis of main"
-timeout 2h bambu ../spec.c  --simulator=VERILATOR  --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=../test.xml --experimental-setup=BAMBU --no-iob -v2
+timeout 2h bambu $root_dir/spec.c  --simulator=VERILATOR  --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=$root_dir/test.xml --experimental-setup=BAMBU --no-iob -v2
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
