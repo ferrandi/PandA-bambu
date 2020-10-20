dddf246a58f022f6a1c95c4ce4587fcfcf14855a
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2020-10-20 13:05:21 +0200

    Fixed an inconsistency between the testebench xml and the crc spec.

--- examples/crc/bambu_altera_NN.sh
+++ examples/crc/bambu_altera_NN.sh
@@ -4,12 +4,20 @@ export PATH=/opt/panda/bin:$PATH
 mkdir -p icrc
 cd icrc
 echo "#synthesis of icrc"
-timeout 2h bambu ../spec.c --top-fname=icrc --simulator=VERILATOR --device-name=EP2C70F896C6-R --simulate --generate-tb=../test_icrc.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU -v2 2>&1 | tee icrc.log
+timeout 2h bambu ../spec.c --top-fname=icrc --simulator=VERILATOR --device-name=EP2C70F896C6-R --simulate --generate-tb=../test_icrc.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU -v2
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
 cd ..
 
 mkdir -p main
 cd main
 echo "#synthesis of main"
-timeout 2h bambu ../spec.c  --simulator=VERILATOR  --device-name=EP2C70F896C6-R --simulate --generate-tb=../test.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU -v2 2>&1 | tee main.log
+timeout 2h bambu ../spec.c  --simulator=VERILATOR  --device-name=EP2C70F896C6-R --simulate --generate-tb=../test.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU -v2
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
 
 
--- examples/crc/bambu_yosys_NN.sh
+++ examples/crc/bambu_yosys_NN.sh
@@ -4,12 +4,20 @@ export PATH=/opt/panda/bin:$PATH
 mkdir -p icrc
 cd icrc
 echo "#synthesis of icrc"
-timeout 2h bambu ../spec.c --top-fname=icrc --simulator=VERILATOR --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=../test_icrc.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU --no-iob -v2 2>&1 | tee icrc.log
+timeout 2h bambu ../spec.c --top-fname=icrc --simulator=VERILATOR --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=../test_icrc.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU --no-iob -v2
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
 cd ..
 
 mkdir -p main
 cd main
 echo "#synthesis of main"
-timeout 2h bambu ../spec.c  --simulator=VERILATOR --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=../test.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU --no-iob -v2 2>&1 | tee main.log
+timeout 2h bambu ../spec.c  --simulator=VERILATOR --device-name=xc7z020,-1,clg484,YOSYS-VVD --simulate --generate-tb=../test.xml --channels-type=MEM_ACC_NN --experimental-setup=BAMBU --no-iob -v2
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
 
 
