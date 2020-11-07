b686a626aba2293d58c498012179a4a08dc4145d
Michele Fiorito <michele.fiorito@polimi.it>
2020-11-07 16:53:44 +0100

    Float quantization examples updated
    Signed-off-by: Michele Fiorito <michele.fiorito@polimi.it>

--- examples/example.sh
+++ examples/example.sh
@@ -6,6 +6,11 @@ return_value=$?
 if test $return_value != 0; then
    exit $return_value
 fi
+$dir_script/floatrange/all.sh $@
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
 $dir_script/libm/all.sh $@
 return_value=$?
 if test $return_value != 0; then
