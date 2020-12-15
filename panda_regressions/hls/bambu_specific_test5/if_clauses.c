26d3b0c650d55a9029958bfa1008c30049ce901e
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2020-12-15 18:15:44 +0100

    Fixed a regression testcase.

--- panda_regressions/hls/bambu_specific_test5/if_clauses.c
+++ panda_regressions/hls/bambu_specific_test5/if_clauses.c
@@ -1,4 +1,4 @@
-int int adders(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l)
+int adders(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l)
 {
    #pragma HLS_simple_pipeline
    
