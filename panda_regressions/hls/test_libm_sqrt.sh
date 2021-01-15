b4abc70d558a3acc79c553e354bbe4046053cdaf
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-01-15 17:44:25 +0100

    Fixed the sqrt test script.

--- panda_regressions/hls/test_libm_sqrt.sh
+++ panda_regressions/hls/test_libm_sqrt.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 abs_script=$(readlink -e $0)
 dir_script=$(dirname $abs_script)
-if test -f output_test_libm_sqrtf/finished; then
+if test -f output_test_libm_sqrt/finished; then
    exit 0
 fi
-rm -fr output_test_libm_sqrtf
-mkdir output_test_libm_sqrtf
-cd output_test_libm_sqrtf
-gcc -fopenmp -O3 -I$dir_script/../../etc/libbambu/ $dir_script/../../etc/libbambu/libm/poly_sqrtf.c -DCHECK_SQRT_FUNCTION -lm -lmpfr -lgmp
+rm -fr output_test_libm_sqrt
+mkdir output_test_libm_sqrt
+cd output_test_libm_sqrt
+gcc -fopenmp -O3 -I$dir_script/../../etc/libbambu/ $dir_script/../../etc/libbambu/libm/poly_sqrt.c -DCHECK_SQRT_FUNCTION -lm 
 ./a.out
 cd ..
 return_value=$?
 if test $return_value != 0; then
-   echo "C based test of softfloat sqrtf not passed."
+   echo "C based test of softfloat sqrt not passed."
    exit $return_value
 fi
-touch output_test_libm_sqrtf/finished
+touch output_test_libm_sqrt/finished
 exit 0
