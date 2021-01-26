8616ba9fb201b96412de19fc18ff4de0b07f72ae
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-01-26 16:01:11 +0100

    Fixed an issue on regression scripts.

--- panda_regressions/hls/test_libm_sqrtf.sh
+++ panda_regressions/hls/test_libm_sqrtf.sh
@@ -9,8 +9,8 @@ mkdir output_test_libm_sqrtf
 cd output_test_libm_sqrtf
 gcc -fopenmp -O3 -I$dir_script/../../etc/libbambu/ $dir_script/../../etc/libbambu/libm/poly_sqrtf.c -DCHECK_SQRT_FUNCTION -lm -lmpfr -lgmp
 ./a.out
-cd ..
 return_value=$?
+cd ..
 if test $return_value != 0; then
    echo "C based test of softfloat sqrtf not passed."
    exit $return_value
