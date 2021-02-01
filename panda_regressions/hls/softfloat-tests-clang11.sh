2a3f53b0e433618b8f24015d2554b63caa6c8b97
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-02-01 08:43:10 +0100

    Fixed regression scripts.

--- panda_regressions/hls/softfloat-tests-clang11.sh
+++ panda_regressions/hls/softfloat-tests-clang11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 $(dirname $0)/../../etc/scripts/test_panda.py --tool=bambu \
-             --args="--configuration-name=softfloat-tests-CLANG10  --soft-float --compiler=I386_CLANG10 --max-ulp=0 --experimental-setup=BAMBU-PERFORMANCE-MP"\
-             -lsoftfloat-tests_list -o output_softfloat-testsCLANG10 -b$(dirname $0) --table=softfloat-testsCLANG10.tex --name="softfloat-testsCLANG10" $@
+             --args="--configuration-name=softfloat-tests-CLANG11  --soft-float --compiler=I386_CLANG11 --max-ulp=0 --experimental-setup=BAMBU-PERFORMANCE-MP"\
+             -lsoftfloat-tests_list -o output_softfloat-testsCLANG11 -b$(dirname $0) --table=softfloat-testsCLANG11.tex --name="softfloat-testsCLANG11" $@
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
