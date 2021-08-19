cfde13ba3a6eafbc63f658d424752cf9b3525a54
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-08-19 16:30:55 +0200

    Updated some results.

--- examples/hls_study/5SGXEA7N2F45C1_02.5_alignedaccess.sh
+++ examples/hls_study/5SGXEA7N2F45C1_02.5_alignedaccess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script=$(readlink -e $0)
 root_dir=$(dirname $script)
-ARGS="--simulator=MODELSIM --compiler=I386_GCC49 --evaluation --no-iob --device-name=5SGXEA7N2F45C1 --print-dot --do-not-expose-globals --aligned-access --clock-period=2.5 --speculative-sdc-scheduling -fno-delete-null-pointer-checks  --panda-parameter=simple-benchmark-name=1"
+ARGS="--simulator=MODELSIM --compiler=I386_GCC49 --evaluation --no-iob --device-name=5SGXEA7N2F45C1 --print-dot --aligned-access --clock-period=2.5 --speculative-sdc-scheduling -fno-delete-null-pointer-checks  --panda-parameter=simple-benchmark-name=1"
 NAME=$(basename $0 .sh)
 DIRNAME=${root_dir##*/}
 $root_dir/../../etc/scripts/test_panda.py --spider-style="$root_dir/latex_format_bambu_results_altera_synth.xml" --tool=bambu -l$root_dir/list-VVD-def --args="$ARGS" -t120m --benchmarks_root=$root_dir -ooutput_${DIRNAME}_$NAME --name=${DIRNAME}_$NAME --table=${DIRNAME}_$NAME.tex "$@"
