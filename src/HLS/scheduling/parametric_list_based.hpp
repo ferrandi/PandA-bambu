7906ee8bde0f8d5ea1ebe732074e4bbf1a49a597
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-08-23 16:42:34 +0200

    Fixed a timing issue when chaining is applied to called functions.

--- src/HLS/scheduling/parametric_list_based.hpp
+++ src/HLS/scheduling/parametric_list_based.hpp
@@ -275,7 +275,7 @@ class parametric_list_based : public Scheduling
                                       CustomMap<std::pair<unsigned int, unsigned int>, double>& local_connection_map, double current_cycle_starting_time, double current_cycle_ending_time, double setup_hold_time, double& phi_extra_time,
                                       double scheduling_mux_margins, bool unbounded, bool unbounded_Functions, bool nonDirectLoadStore, const std::set<std::string>& proxy_functions_used, bool cstep_has_RET_conflict, unsigned int fu_type,
                                       const vertex2obj<ControlStep>& current_ASAP, const fu_bindingRef res_binding, const ScheduleRef schedule, bool& predecessorsCond, bool& pipeliningCond, bool& cannotBeChained0, bool& chainingRetCond,
-                                      bool& cannotBeChained1, bool& asyncCond, bool& cannotBeChained2, bool& MultiCond0, bool& MultiCond1, bool& nonDirectMemCond, bool& unboundedFunctionsCond, bool& proxyFunCond);
+                                      bool& cannotBeChained1, bool& asyncCond, bool& cannotBeChained2, bool& cannotBeChained3, bool& MultiCond0, bool& MultiCond1, bool& nonDirectMemCond, bool& unboundedFunctionsCond, bool& proxyFunCond);
    /**
     * Compute the relationship of this step
     * @param relationship_type is the type of relationship to be considered
