f4341bc619454d8bee34ae544c489061bad93ef4
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-08-24 08:48:23 +0200

    Fixed bambu_specific_test4 script.

--- panda_regressions/hls/bambu_specific_test4.sh
+++ panda_regressions/hls/bambu_specific_test4.sh
@@ -5,7 +5,7 @@ OUT_SUFFIX="bambu_specific_test4"
 
 $(dirname $0)/../../etc/scripts/test_panda.py --tool=bambu  \
    --args="--configuration-name=O2-INFER -O2 --experimental-setup=BAMBU ${BATCH_ARGS[*]}" \
-   --args="--configuration-name=O2-INFER-MP -O2 --experimental-setup=BAMBU-BALANCED-MP -funroll-loops ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=O2-INFER-MP -O2 --experimental-setup=BAMBU-BALANCED-MP ${BATCH_ARGS[*]}" \
    -lbambu_specific_test4_list \
    -o "output_${OUT_SUFFIX}" -b$(dirname $0) \
    --table="${OUT_SUFFIX}.tex" \
