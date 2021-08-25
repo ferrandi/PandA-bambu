3984003e57514d0a188c8f33e03b80a76630ca18
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-08-25 21:51:35 +0200

    Added a new test.

--- panda_regressions/hls/bambu_specific_test.sh
+++ panda_regressions/hls/bambu_specific_test.sh
@@ -4,9 +4,7 @@ BATCH_ARGS=("-lm" "--simulate" "--experimental-setup=BAMBU" "--compiler=I386_GCC
 OUT_SUFFIX="bambu_specific_test"
 
 $(dirname $0)/../../etc/scripts/test_panda.py --tool=bambu  \
-   --args="--configuration-name=O0 -O0 ${BATCH_ARGS[*]}" \
-   --args="--configuration-name=O1 -O1 ${BATCH_ARGS[*]}" \
-   --args="--configuration-name=O2 -O2 ${BATCH_ARGS[*]}" \
+   --args="${BATCH_ARGS[*]}" \
    -lbambu_specific_test_list \
    -o "output_${OUT_SUFFIX}" -b$(dirname $0) \
    --table="${OUT_SUFFIX}.tex" \
@@ -14,4 +12,3 @@ $(dirname $0)/../../etc/scripts/test_panda.py --tool=bambu  \
    --name="${OUT_SUFFIX}" "$@"
 exit $?
 
-#   --args="--configuration-name=O3 -O3 ${BATCH_ARGS[*]}" 
