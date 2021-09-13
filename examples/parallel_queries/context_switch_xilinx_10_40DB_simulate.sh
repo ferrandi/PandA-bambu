778b753a036ae3bb743841a1afdf9445ebbe4a56
Michele Fiorito <michele.fiorito@polimi.it>
2021-09-13 23:27:38 +0200

    Command typo fixed in example scripts

--- examples/parallel_queries/context_switch_xilinx_10_40DB_simulate.sh
+++ examples/parallel_queries/context_switch_xilinx_10_40DB_simulate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COMMONARGS="--simulator=MODELSIM --compiler=I386_GCC49 --std=c99 --experimental-set=BAMBU -O3 -fno-delete-null-pointer-checks -fopenmp --pragma-parse --mem-delay-read=20 --mem-delay-write=20 --channels-type=MEM_ACC_11 --memory-allocation-policy=NO_BRAM --no-iob --device-name=xc7vx690t-3ffg1930-VVD --clock-period=10 -DMAX_VERTEX_NUMBER=1309073 -DMAX_EDGE_NUMBER=5309056 --max-sim-cycles=2000000000 --simulate"
+COMMONARGS="--simulator=MODELSIM --compiler=I386_GCC49 --std=c99 --experimental-setup=BAMBU -O3 -fno-delete-null-pointer-checks -fopenmp --pragma-parse --mem-delay-read=20 --mem-delay-write=20 --channels-type=MEM_ACC_11 --memory-allocation-policy=NO_BRAM --no-iob --device-name=xc7vx690t-3ffg1930-VVD --clock-period=10 -DMAX_VERTEX_NUMBER=1309073 -DMAX_EDGE_NUMBER=5309056 --max-sim-cycles=2000000000 --simulate"
  NAME=`basename $0 .sh`
 `dirname $0`/../../etc/scripts/test_panda.py --spider-style="../lib/latex_format_bambu_results_xilinx.xml" --tool=bambu -l`dirname $0`/list_40DB -ooutput_$NAME --commonargs="$COMMONARGS" \
       --args="--configuration-name=02W-04CH-2C-01CS -DN_THREADS=2  --num-accelerators=2  --memory-banks-number=4  --channels-number=2 --context_switch=1"\
