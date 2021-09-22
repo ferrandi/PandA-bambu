fe603b890aa96b18cd58f1b2b3f4c31144a4254b
Michele Fiorito <michele.fiorito@polimi.it>
2021-09-22 22:50:55 +0200

    GCC builder workflow updated

--- .github/actions/build-gcc/entrypoint.sh
+++ .github/actions/build-gcc/entrypoint.sh
@@ -2,14 +2,26 @@
 set -e
 
 BRANCH=$1
+PATCH_FILE="$(compgen -G gcc-*.patch || echo '')"
 DIST_NAME="$2-$(lsb_release -is)_$(lsb_release -rs)"
 DIST_DIR="$GITHUB_WORKSPACE/$DIST_NAME"
 shift
 shift
 
+workspace_dir=$PWD
+
+function cleanup {
+   cd $workspace_dir
+   rm -rf gcc
+}
+trap cleanup EXIT
+
 git clone --depth 1 --branch $BRANCH git://gcc.gnu.org/git/gcc.git
 
 cd gcc
+if [[ -f "../$PATCH_FILE" ]]; then
+   git apply ../$PATCH_FILE
+fi
 ./contrib/download_prerequisites
 mkdir build
 cd build
