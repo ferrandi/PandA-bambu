635a04bded993f78b952ad64f843f7c71d7bc011
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-10-04 20:30:48 +0200

    Fixed NanoXplore arf_res_sharing scripts.

--- examples/arf_res_sharing/unconstrained_synth_nanoxplore_nx1h140tsp.sh
+++ examples/arf_res_sharing/unconstrained_synth_nanoxplore_nx1h140tsp.sh
@@ -4,8 +4,8 @@ root_dir=$(dirname $script)
 
 mkdir -p unconstrained_synth_nanoxplore_nx1h140tsp
 cd unconstrained_synth_nanoxplore_nx1h140tsp
-echo "# Diamond synthesis and ICARUS simulation"
-timeout 2h bambu $root_dir/module.c --generate-tb=$root_dir/test.xml --simulator=ICARUS --device-name=nx1h140tsp --evaluation --experimental-setup=BAMBU --generate-interface=WB4 --cprf=0.9 --skip-pipe-parameter=1
+echo "# NanoXplore synthesis and ICARUS simulation"
+timeout 2h bambu $root_dir/module.c --generate-tb=$root_dir/test.xml --simulator=ICARUS --device-name=nx1h140tsp --evaluation --experimental-setup=BAMBU --generate-interface=WB4 --cprf=0.9 --skip-pipe-parameter=1 "$@"
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
