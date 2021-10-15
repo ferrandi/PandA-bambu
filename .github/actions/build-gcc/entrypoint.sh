28b2fb9627ed0bafcddbadab37cf6179f501bc79
Michele Fiorito <michele.fiorito@polimi.it>
2021-10-15 16:08:41 +0200

    Long workflows output names fixed

--- .github/actions/build-gcc/entrypoint.sh
+++ .github/actions/build-gcc/entrypoint.sh
@@ -9,7 +9,7 @@ shift
 shift
 
 workspace_dir=$PWD
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH
+export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
 
 function cleanup {
    cd $workspace_dir
