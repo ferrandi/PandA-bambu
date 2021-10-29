b743405de18e0a5e9b5bfc7eb63d5f108a123847
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-10-29 10:52:00 +0200

    Fixed LLVM based tutorial exercise.

--- documentation/tutorial_ics_2021/01-introduction/Exercise3/bambu-clang12.sh
+++ documentation/tutorial_ics_2021/01-introduction/Exercise3/bambu-clang12.sh
@@ -5,8 +5,9 @@ root_dir=$(dirname $script)
 rm -rf keccak_V7
 mkdir keccak_V7
 cd keccak_V7
-bambu $root_dir/Keccak.c --top-fname=kekka_coproc \
+clang-12 -O3 -fno-slp-vectorize -fno-vectorize $root_dir/Keccak.c -emit-llvm -S -o test.ll
+bambu test.ll --top-fname=kekka_coproc \
    --clock-period=2.5 --device-name=xc7vx690t-3ffg1930-VVD \
    --generate-tb=$root_dir/test.xml --simulate \
    --compiler=I386_CLANG12 --no-iob \
-   --print-dot -v4 "$@" |& tee log.txt
+   --print-dot --pretty-print=a.c -v4 "$@" |& tee log.txt
