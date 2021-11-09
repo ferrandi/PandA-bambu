b058e46daad5c0ff792966bd93f9c96cf9315b75
Michele Fiorito <michele.fiorito@polimi.it>
2021-11-09 11:28:25 +0100

    GCC regression exception extended

--- panda_regressions/hls/gcc_regression_simple/930608-1.c
+++ panda_regressions/hls/gcc_regression_simple/930608-1.c
@@ -3,7 +3,7 @@ double (* const a[]) (double) = {&f};
 
 main ()
 {
-#if (__GNUC__ != 4 || __GNUC_MINOR__ != 7)
+#if (__GNUC__ != 4 || __GNUC_MINOR__ >= 7)
   double (*p) ();
   p = &f;
   if (p != a[0])
