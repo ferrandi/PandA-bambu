90777df94564ef02b4bb9acac2325f6ab3762c9c
Michele Fiorito <michele.fiorito@polimi.it>
2021-11-11 19:06:27 +0100

    AppImage build information updated

--- .github/actions/generate-bin/entrypoint.sh
+++ .github/actions/generate-bin/entrypoint.sh
@@ -1,3 +1,21 @@
+###
+#     AppImage builder script
+#
+#     The following script needs to be exectued from PandA Bambu repository root and 
+#     take as arguments the configure options for the PandA Bambu build.
+#     An appimage executable will be generated as an output.
+#     Consider that the script will share the whole repository with the container.
+#     
+#     The script will look for frontend compilers in into the ./dist and ./compiler 
+#     directories. All compiler binaries and librares found inside the aforementioned 
+#     directories will be copied into the root filesystem of the container, while only 
+#     the ones from ./dist directory will be included into the generated AppImage.
+#     When a compiler is added to the ./compiler directory only, it will not be available 
+#     inside the final AppImage, thus it is important to exclude it during PandA Bambu
+#     configuration, passing the correct configuration option (e.g. --with-gcc8=/bin/false
+#     if gcc-8 is not to be cosidered during PandA Bambu compilation).
+#
+###
 #!/bin/bash
 set -e
 
