ea16a73d915918c1f3078ff4c95f9bb2c7c21382
Michele Fiorito <michele.fiorito@polimi.it>
2021-11-11 11:28:50 +0100

    GCC regression test 930608-1 issue solved

--- panda_regressions/hls/gcc_regression_simple/930608-1.c
+++ panda_regressions/hls/gcc_regression_simple/930608-1.c
@@ -3,11 +3,9 @@ double (* const a[]) (double) = {&f};
 
 main ()
 {
-#if (__GNUC__ != 4 || __GNUC_MINOR__ >= 7)
   double (*p) ();
   p = &f;
   if (p != a[0])
     abort ();
-#endif
   exit (0);
 }
