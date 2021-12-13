7ec1196e773d4e261657e13b45d49e364eb62cca
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2021-12-13 18:19:18 +0100

    OpenRoad reworked.

---
+++ etc/devices/Generic_devices/nangate45/config.mk
@@ -0,0 +1,119 @@
+# Process node
+export PROCESS = 45
+
+#-----------------------------------------------------
+# Tech/Libs
+# ----------------------------------------------------
+export TECH_LEF = $(PLATFORM_DIR)/lef/NangateOpenCellLibrary.tech.lef
+export SC_LEF = $(PLATFORM_DIR)/lef/NangateOpenCellLibrary.macro.mod.lef
+
+export LIB_FILES = $(PLATFORM_DIR)/lib/NangateOpenCellLibrary_typical.lib \
+                     $(ADDITIONAL_LIBS)
+export GDS_FILES = $(sort $(wildcard $(PLATFORM_DIR)/gds/*.gds)) \
+                     $(ADDITIONAL_GDS)
+# Dont use cells to ease congestion
+# Specify at least one filler cell if none
+export DONT_USE_CELLS = TAPCELL_X1 FILLCELL_X1 AOI211_X1 OAI211_X1
+
+# Fill cells used in fill cell insertion
+export FILL_CELLS = FILLCELL_X1 FILLCELL_X2 FILLCELL_X4 FILLCELL_X8 FILLCELL_X16 FILLCELL_X32
+
+# -----------------------------------------------------
+#  Yosys
+#  ----------------------------------------------------
+# Set the TIEHI/TIELO cells
+# These are used in yosys synthesis to avoid logical 1/0's in the netlist
+export TIEHI_CELL_AND_PORT = LOGIC1_X1 Z
+export TIELO_CELL_AND_PORT = LOGIC0_X1 Z
+
+# Used in synthesis
+export MIN_BUF_CELL_AND_PORTS = BUF_X1 A Z
+
+# Used in synthesis
+export MAX_FANOUT = 100
+
+# Yosys mapping files
+export LATCH_MAP_FILE = $(PLATFORM_DIR)/cells_latch.v
+export CLKGATE_MAP_FILE = $(PLATFORM_DIR)/cells_clkgate.v
+export ADDER_MAP_FILE ?= $(PLATFORM_DIR)/cells_adders.v
+#
+# Set yosys-abc clock period to first "-period" found in sdc file
+export ABC_CLOCK_PERIOD_IN_PS ?= $(shell grep -E -o -m 1 "\-period\s+\S+" $(SDC_FILE) | awk '{print $$2*1000}')
+export ABC_DRIVER_CELL = BUF_X1
+# BUF_X1, pin (A) = 0.974659. Arbitrarily multiply by 4
+export ABC_LOAD_IN_FF = 3.898
+
+#--------------------------------------------------------
+# Floorplan
+# -------------------------------------------------------
+
+# Placement site for core cells
+# This can be found in the technology lef
+export PLACE_SITE = FreePDK45_38x28_10R_NP_162NW_34O
+#
+# IO Pin fix margin
+export IO_PIN_MARGIN = 70
+#
+# IO Placer pin layers
+export IO_PLACER_H = metal3
+export IO_PLACER_V = metal2
+
+# Define default PDN config
+export PDN_CFG ?= $(PLATFORM_DIR)/pdn.cfg
+
+# Endcap and Welltie cells
+export TAPCELL_TCL = $(PLATFORM_DIR)/tapcell.tcl
+
+export MACRO_PLACE_HALO ?= 22.4 15.12
+export MACRO_PLACE_CHANNEL ?= 18.8 19.95
+
+#---------------------------------------------------------
+# Place
+# --------------------------------------------------------
+# Layer to use for parasitics estimations
+export WIRE_RC_LAYER = metal3
+
+# Cell padding in SITE widths to ease rout-ability.  Applied to both sides
+export CELL_PAD_IN_SITES_GLOBAL_PLACEMENT ?= 2
+export CELL_PAD_IN_SITES_DETAIL_PLACEMENT ?= 1
+#
+# resizer repair_long_wires -max_length
+export MAX_WIRE_LENGTH = 1000
+
+export PLACE_DENSITY ?= 0.30
+
+# --------------------------------------------------------
+#  CTS
+#  -------------------------------------------------------
+# TritonCTS options
+export CTS_BUF_CELL   = BUF_X4
+export CTS_TECH_DIR   = $(PLATFORM_DIR)/tritonCTS
+
+
+# ---------------------------------------------------------
+#  Route
+# ---------------------------------------------------------
+# FastRoute options
+export MIN_ROUTING_LAYER = metal2
+export MAX_ROUTING_LAYER = metal10
+
+# Define fastRoute tcl
+export FASTROUTE_TCL = $(PLATFORM_DIR)/fastroute.tcl
+
+
+# KLayout technology file
+export KLAYOUT_TECH_FILE = $(PLATFORM_DIR)/FreePDK45.lyt
+
+# KLayout DRC ruledeck
+export KLAYOUT_DRC_FILE = $(PLATFORM_DIR)/drc/FreePDK45.lydrc
+
+# KLayout LVS ruledeck
+export KLAYOUT_LVS_FILE = $(PLATFORM_DIR)/lvs/FreePDK45.lylvs
+
+export CDL_FILE = $(PLATFORM_DIR)/cdl/NangateOpenCellLibrary.cdl
+
+# Template definition for power grid analysis
+export TEMPLATE_PGA_CFG ?= $(PLATFORM_DIR)/template_pga.cfg
+
+# OpenRCX extRules
+export RCX_RULES                           = ./platforms/$(PLATFORM)/rcx_patterns.rules
---
+++ etc/devices/Generic_devices/script/pdn.tcl
@@ -0,0 +1,40 @@
+if {![info exists standalone] || $standalone} {
+  # Read lef
+  read_lef $::env(TECH_LEF)
+  read_lef $::env(SC_LEF)
+  if {[info exist ::env(ADDITIONAL_LEFS)]} {
+    foreach lef $::env(ADDITIONAL_LEFS) {
+      read_lef $lef
+    }
+  }
+
+  # Read liberty files
+  source $::env(SCRIPTS_DIR)/read_liberty.tcl
+
+  # Read design files
+  read_def $::env(RESULTS_DIR)/2_5_floorplan_tapcell.def
+} else {
+  puts "Starting PDN generation"
+}
+
+if {[info exist ::env(PDN_TCL)]} {
+  source $::env(PDN_TCL)
+  pdngen -verbose
+} elseif {[info exist ::env(PDN_CFG)]} {
+  pdngen $::env(PDN_CFG) -verbose
+}
+
+#foreach net_name [concat $::power_nets $::ground_nets] {
+#  check_power_grid -net $net_name
+#}
+
+if {![info exists standalone] || $standalone} {
+  # write output
+  write_def $::env(RESULTS_DIR)/2_6_floorplan_pdn.def
+
+  if {[info exists ::env(POST_PDN_TCL)] && [file exists $::env(POST_PDN_TCL)]} {
+    source $::env(POST_PDN_TCL)
+  }
+
+  exit
+}
