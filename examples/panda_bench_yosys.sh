21684e5035df2a0f48c74f942bdaf0bba537cb2a
Michele Fiorito <michele.fiorito@polimi.it>
2021-12-16 15:27:17 +0100

    ASIC synthesis regression tests added

---
+++ examples/panda_bench_yosys.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+script_dir="$(dirname $(readlink -e $0))"
+BATCH_ARGS=("--no-iob" "--clock-period=5" "--compiler=I386_CLANG12" "--evaluation")
+OUT_SUFFIX="pb_yosys"
+
+$script_dir/../etc/scripts/test_panda.py --tool=bambu \
+   --args="--configuration-name=nangate45 --device-name=nangate45 BENCHMARK_ENV=DIE_AREA='0 0 2020 1980',CORE_AREA='10 12 2010 1971' ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=asap7-BC  --device-name=asap7-BC  BENCHMARK_ENV=DIE_AREA='0 0 190 190',CORE_AREA='5.08 5.08 180 180' ${BATCH_ARGS[*]}" \
+   -lpanda_bench_yosys_list \
+   -o "out${OUT_SUFFIX}" -b$script_dir \
+   --name="${OUT_SUFFIX}" "$@"
