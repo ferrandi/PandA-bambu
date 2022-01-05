0fd0084799e319d87ff8315984c9deef8d97cabc
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2022-01-05 13:39:31 +0100

    Copyright updated.

--- src/HLS/function_allocation/omp_function_allocation_CS.hpp
+++ src/HLS/function_allocation/omp_function_allocation_CS.hpp
@@ -12,7 +12,7 @@
  *                       Politecnico di Milano - DEIB
  *                        System Architectures Group
  *             ***********************************************
- *              Copyright (c) 2015-2021 Politecnico di Milano
+ *              Copyright (c) 2015-2022 Politecnico di Milano
  *
  *   This file is part of the PandA framework.
  *
