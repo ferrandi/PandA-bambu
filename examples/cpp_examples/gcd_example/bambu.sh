9ee7fea8782e7993131bb2dd227d6b6f21edc5de
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2022-01-11 11:32:52 +0100

    Improved regression on cpp files.

--- examples/cpp_examples/gcd_example/bambu.sh
+++ examples/cpp_examples/gcd_example/bambu.sh
@@ -2,11 +2,20 @@
 script=$(readlink -e $0)
 root_dir=$(dirname $script)
 basename_dir=$(basename $root_dir)
-
-mkdir -p $basename_dir/hls
-cd $basename_dir/hls
-echo "#synthesis and simulation"
-timeout 2h bambu --simulator=MODELSIM --print-dot $root_dir/gcd.cc --compiler=I386_GCC8 --generate-tb=$root_dir/test.xml --no-iob --evaluation --discrepancy --top-fname=gcd "$@"
+current_dir=$(pwd)
+mkdir -p $basename_dir/hls1
+cd $basename_dir/hls1
+echo "#simulation and discrepancy"
+timeout 2h bambu --simulator=MODELSIM --print-dot $root_dir/gcd.cc --compiler=I386_GCC8 --generate-tb=$root_dir/test.xml --no-iob --simulate --discrepancy --top-fname=gcd "$@"
+return_value=$?
+if test $return_value != 0; then
+   exit $return_value
+fi
+cd $current_dir
+mkdir -p $basename_dir/hls2
+cd $basename_dir/hls2
+echo "#just simulation"
+timeout 2h bambu --simulator=MODELSIM --print-dot $root_dir/gcd.cc --compiler=I386_GCC8 --generate-tb=$root_dir/test.xml --no-iob --simulate --top-fname=gcd "$@"
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
