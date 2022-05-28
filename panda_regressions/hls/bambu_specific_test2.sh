3d8b6f6a185f3b9a8e5022396a83c13f75997e36
Michele Fiorito <michele.fiorito@polimi.it>
2022-05-28 02:21:45 +0200

    Regression scripts fix

--- panda_regressions/hls/bambu_specific_test2.sh
+++ panda_regressions/hls/bambu_specific_test2.sh
@@ -5,14 +5,14 @@ BATCH_ARGS=("-lm" "--simulate" "--expose-globals")
 OUT_SUFFIX="bambu_specific_test2"
 
 python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu  \
-   --args="--compiler=I386_GCC49 --configuration-name=GCC49_O0 -O0 ${BATCH_ARGS[*]}" \
-   --args="--compiler=I386_GCC49 --configuration-name=GCC49_O1 -O1 ${BATCH_ARGS[*]}" \
-   --args="--compiler=I386_GCC49 --configuration-name=GCC49_O2 -O2 ${BATCH_ARGS[*]}" \
-   --args="--compiler=I386_GCC49 --configuration-name=GCC49_O3 -O3 ${BATCH_ARGS[*]}" \
-   --args="--compiler=I386_CLANG13 --configuration-name=CLANG13_O0 -O0 ${BATCH_ARGS[*]}" \
-   --args="--compiler=I386_CLANG13 --configuration-name=CLANG13_O1 -O1 ${BATCH_ARGS[*]}" \
-   --args="--compiler=I386_CLANG13 --configuration-name=CLANG13_O2 -O2 ${BATCH_ARGS[*]}" \
-   --args="--compiler=I386_CLANG13 --configuration-name=CLANG13_O3 -O3 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=GCC49_O0 --compiler=I386_GCC49 -O0 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=GCC49_O1 --compiler=I386_GCC49 -O1 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=GCC49_O2 --compiler=I386_GCC49 -O2 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=GCC49_O3 --compiler=I386_GCC49 -O3 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=CLANG13_O0 --compiler=I386_CLANG13 -O0 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=CLANG13_O1 --compiler=I386_CLANG13 -O1 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=CLANG13_O2 --compiler=I386_CLANG13 -O2 ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=CLANG13_O3 --compiler=I386_CLANG13 -O3 ${BATCH_ARGS[*]}" \
    -lbambu_specific_test2_list \
    -o "out_${OUT_SUFFIX}" -b$script_dir \
    --name="${OUT_SUFFIX}" "$@"
