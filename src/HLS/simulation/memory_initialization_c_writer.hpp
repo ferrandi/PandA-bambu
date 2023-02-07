dbf9998dad38b11a3b8ea945bbe483ee612dcb8f
Michele Fiorito <michele.fiorito@polimi.it>
2023-02-07 11:47:00 +0100

    Copyright year update

--- src/HLS/simulation/memory_initialization_c_writer.hpp
+++ src/HLS/simulation/memory_initialization_c_writer.hpp
@@ -12,7 +12,7 @@
  *                       Politecnico di Milano - DEIB
  *                        System Architectures Group
  *             ***********************************************
- *              Copyright (c) 2019-2022 Politecnico di Milano
+ *              Copyright (c) 2019-2023 Politecnico di Milano
  *
  *   This file is part of the PandA framework.
  *
--- src/frontend_analysis/IR_analysis/lut_transformation.hpp
+++ src/frontend_analysis/IR_analysis/lut_transformation.hpp
@@ -12,7 +12,7 @@
  *                              Politecnico di Milano - DEIB
  *                                System Architectures Group
  *                 ***********************************************
- *                  Copyright (C) 2004-2022 Politecnico di Milano
+ *                  Copyright (C) 2004-2023 Politecnico di Milano
  *
  *    This file is part of the PandA framework.
  *
