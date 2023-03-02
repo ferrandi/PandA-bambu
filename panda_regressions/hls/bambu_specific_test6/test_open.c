5273727dc00ef15986c13127ad49998e93e92c2d
Giovanni Gozzi <giovanni.gozzi@mail.polimi.it>
2023-03-02 14:15:33 +0100

    FIxed specific test open and removed unnedeed test

--- panda_regressions/hls/bambu_specific_test6/test_open.c
+++ panda_regressions/hls/bambu_specific_test6/test_open.c
@@ -2,15 +2,36 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-   int fd = open("bin_test.dat", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
-   int number, bytes_read = 0;
-   if(fd != -1)
-   {      
-      bytes_read = read(fd, &number, sizeof(unsigned int));
+   int fd = open("bin_test.dat", O_RDONLY, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+   int number = 0, bytes_read = 0;
+   long long int number2=0;
+   if(fd == -1)
+   {   
+      abort();   
       close(fd);
    }
-   return number;
+   bytes_read = read(fd, &number, sizeof(number));
+   printf("%d\n", number);
+   if(number != 2 || bytes_read != 4)
+   {
+      abort();
+   }
+   bytes_read = read(fd, &number, 1);
+   printf("%d\n", number);
+   if(number != 2 || bytes_read != 1)
+   {
+      abort();
+   }
+   bytes_read = read(fd, &number2,sizeof(number2));
+   printf("%lld\n", number2);
+   if(number2 != 18383834416414720LL || bytes_read != 8)
+   {
+      abort();
+   }
+   return 0;
 }
