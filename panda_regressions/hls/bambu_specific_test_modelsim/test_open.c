7adc093bf088d4b5c89f13c3303e249ebb87b360
Giovanni Gozzi <giovanni.gozzi@mail.polimi.it>
2023-03-03 11:21:01 +0100

    Moved specific open test that requires modelsim

--- panda_regressions/hls/bambu_specific_test_modelsim/test_open.c
+++ panda_regressions/hls/bambu_specific_test_modelsim/test_open.c
@@ -16,19 +16,16 @@ int main()
       close(fd);
    }
    bytes_read = read(fd, &number, sizeof(number));
-   printf("%d\n", number);
    if(number != 2 || bytes_read != 4)
    {
       abort();
    }
    bytes_read = read(fd, &number, 1);
-   printf("%d\n", number);
    if(number != 2 || bytes_read != 1)
    {
       abort();
    }
    bytes_read = read(fd, &number2,sizeof(number2));
-   printf("%lld\n", number2);
    if(number2 != 18383834416414720LL || bytes_read != 8)
    {
       abort();
