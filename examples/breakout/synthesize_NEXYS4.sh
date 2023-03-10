1b22f4c6342f83486a9d0296a0bc6729306814a1
Michele Fiorito <Ansaya@users.noreply.github.com>
2023-03-10 09:46:32 +0100

    IOBs connection default option inverted (#136)
    * IOBs connection default option inverted
    Now IOBs are disconnected by default.
    The option name has been changed to --connect-iob

--- examples/breakout/synthesize_NEXYS4.sh
+++ examples/breakout/synthesize_NEXYS4.sh
@@ -5,7 +5,7 @@ rm -rf breakout_synth
 mkdir -p breakout_synth
 cd breakout_synth
 timeout 2h bambu $script_dir/main.c --top-fname=main \
-   --compiler=I386_CLANG11 -Os --device-name=xc7a100t-1csg324-VVD --clock-period=10 \
+   --compiler=I386_CLANG11 -Os --device-name=xc7a100t-1csg324-VVD --clock-period=10 --connect-iob \
    --backend-sdc-extensions=$script_dir/Nexys4_Master.sdc $script_dir/constraints_STD.xml $script_dir/IPs.xml \
    --C-no-parse=$script_dir/plot.c,$script_dir/sevensegments_ctrl.c,$script_dir/btn_ctrl.c,$script_dir/get_ticks.c \
    --file-input-data=$script_dir/plot.v,$script_dir/sevensegments_ctrl.v,$script_dir/btn_ctrl.v,$script_dir/get_ticks.v \
--- examples/taste/taste.sh
+++ examples/taste/taste.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir="$(dirname $(readlink -e $0))"
 python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu \
-   --args="--simulator=MODELSIM --evaluation=AREA" \
+   --args="--simulator=MODELSIM --evaluation=AREA --connect-iob" \
    -ltaste_list -o output_taste -b$script_dir --table=TASTEoutput.tex --name="TASTE" "$@"
 return_value=$?
 if test $return_value != 0; then
