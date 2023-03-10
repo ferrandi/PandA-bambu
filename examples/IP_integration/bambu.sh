9da1bacf51d65dcbd61a65c0f8836419ee2f590a
Michele Fiorito <Ansaya@users.noreply.github.com>
2023-03-10 09:46:32 +0100

    IOBs connection default option inverted (#136)
    * IOBs connection default option inverted
    Now IOBs are disconnected by default.
    The option name has been changed to --connect-iob

--- examples/IP_integration/bambu.sh
+++ examples/IP_integration/bambu.sh
@@ -6,7 +6,7 @@ mkdir -p IP_integration_hls
 cd IP_integration_hls
 echo "#synthesis with VIVADO RTL and simulation"
 timeout 2h bambu $root_dir/main_test.c $root_dir/top.c $root_dir/module_lib.xml --top-fname=main --top-rtldesign-name=my_ip \
-   $root_dir/constraints_STD.xml --experimental-setup=BAMBU --no-iob --memory-allocation-policy=ALL_BRAM -O3 \
+   $root_dir/constraints_STD.xml --experimental-setup=BAMBU --memory-allocation-policy=ALL_BRAM -O3 \
    --C-no-parse=$root_dir/module1.c,$root_dir/module2.c,$root_dir/printer1.c,$root_dir/printer2.c \
    --file-input-data=$root_dir/module1.v,$root_dir/module2.v,$root_dir/printer1.v,$root_dir/printer2.v \
    --generate-tb=$root_dir/test.xml --simulator=MODELSIM --print-dot --evaluation -DBAMBU_PROFILING "$@"
