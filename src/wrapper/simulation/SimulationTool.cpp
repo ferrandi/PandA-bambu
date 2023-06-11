a68930a5898e591536458361b61cf1f614512e98
Michele Fiorito <michele.fiorito@polimi.it>
2023-06-12 00:09:56 +0200

    Co-simulation pretty print compilation fix PIC symbols needs should still be global after symbols localization

--- src/wrapper/simulation/SimulationTool.cpp
+++ src/wrapper/simulation/SimulationTool.cpp
@@ -404,10 +404,10 @@ std::string SimulationTool::GenerateLibraryBuildScript(std::ostringstream& scrip
       const auto m_pp_top_fname = add_fname_prefix("__m_pp_");
       const auto pp_file = boost::filesystem::path(Param->getOption<std::string>(OPT_pretty_print));
       const auto pp_fileo = output_dir + "/" + pp_file.stem().string() + ".o";
-      script << "${CC} -c ${CFLAGS} -fvisibility=hidden -fno-strict-aliasing -fPIC -o " << pp_fileo << " "
-             << pp_file.string() << "\n"
-             << "objcopy --localize-hidden " << pp_fileo << "\n"
-             << "objcopy --globalize-symbol " << top_fname << " " << pp_fileo << "\n"
+      script << "${CC} -c ${CFLAGS} -fno-strict-aliasing -fPIC -o " << pp_fileo << " " << pp_file.string() << "\n"
+             << "objcopy --keep-global-symbol " << top_fname << " $(nm " << pp_fileo
+             << " | grep -o '[^[:space:]]*get_pc_thunk[^[:space:]]*' | sed 's/^/--keep-global-symbol /' | tr '\n' ' ') "
+             << pp_fileo << "\n"
              << "objcopy --redefine-sym " << top_fname << "=" << m_pp_top_fname << " " << pp_fileo << "\n"
              << "objs+=(\"" << pp_fileo << "\")\n\n";
    }
