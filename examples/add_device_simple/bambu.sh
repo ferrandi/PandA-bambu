9fa0a20ae6b85f7a24b96f221a1e7f601223515b
Michele Fiorito <michele.fiorito@polimi.it>
2023-06-22 08:57:37 +0200

    Xilinx XSIM tests updated Xilinx XSIM supports 64-bit simulation only

--- examples/add_device_simple/bambu.sh
+++ examples/add_device_simple/bambu.sh
@@ -5,7 +5,7 @@ root_dir=$(dirname $script)
 mkdir -p add_device_simple_sim
 cd add_device_simple_sim
 echo "# HLS synthesis, testbench generation and simulation"
-timeout 2h bambu -O2 $root_dir/module.c --generate-tb=$root_dir/test.xml --evaluation=CYCLES --simulator=XSIM --pretty-print=a.c --experimental-setup=BAMBU --target-file=$root_dir/xc7z045-2ffg900-VVD.xml "$@"
+timeout 2h bambu -O2 -m64 $root_dir/module.c --generate-tb=$root_dir/test.xml --evaluation=CYCLES --simulator=XSIM --pretty-print=a.c --experimental-setup=BAMBU --target-file=$root_dir/xc7z045-2ffg900-VVD.xml "$@"
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
@@ -15,7 +15,7 @@ cd ..
 mkdir -p add_device_simple_synth
 cd add_device_simple_synth
 echo "# HLS synthesis, testbench generation, simulation with XSIM and RTL synthesis with VIVADO RTL"
-timeout 2h bambu -O2 $root_dir/module.c --generate-tb=$root_dir/test.xml --evaluation --simulator=XSIM --pretty-print=a.c --experimental-setup=BAMBU --target-file=$root_dir/xc7z045-2ffg900-VVD.xml "$@"
+timeout 2h bambu -O2 -m64 $root_dir/module.c --generate-tb=$root_dir/test.xml --evaluation --simulator=XSIM --pretty-print=a.c --experimental-setup=BAMBU --target-file=$root_dir/xc7z045-2ffg900-VVD.xml "$@"
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
