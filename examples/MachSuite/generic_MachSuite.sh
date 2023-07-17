ec08363b834ba2cd21a3664ceed7db85839f55bc
Michele Fiorito <michele.fiorito@polimi.it>
2023-07-17 20:25:14 +0200

    Mach Suite test script fix

--- examples/MachSuite/generic_MachSuite.sh
+++ examples/MachSuite/generic_MachSuite.sh
@@ -3,7 +3,7 @@ script_dir="$(dirname $(readlink -e $0))"
 ggo_require_compiler=1
 . $script_dir/../../panda_regressions/hls/generic_getopt.sh
 
-BATCH_ARGS=("--hls-div" "--experimental-setup=BAMBU-PERFORMANCE-MP" "--top-fname=main" "--top-rtldesign-name=run_benchmark" "-mx32" "-fno-tree-vectorize" "--simulate" "--simulator=VERILATOR" "-s" "-DBAMBU_PROFILING")
+BATCH_ARGS=("--hls-div" "--experimental-setup=BAMBU-PERFORMANCE-MP" "--top-fname=run_benchmark" "--top-rtldesign-name=run_benchmark" "-mx32" "-fno-tree-vectorize" "--simulate" "--simulator=VERILATOR" "-s" "-DBAMBU_PROFILING")
 OUT_SUFFIX="${compiler}_MachSuite"
 
 python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu  \
