17e0cb517ca35cd66e52fd549d1ccc0b3742bbb0
Michele Fiorito <michele.fiorito@polimi.it>
2023-07-21 16:58:34 +0200

    MachSuite setup update MachSuite is now included as a submodule; Top-level interface is now inferred from function signature

--- examples/MachSuite/generic_MachSuite.sh
+++ examples/MachSuite/generic_MachSuite.sh
@@ -3,11 +3,12 @@ script_dir="$(dirname $(readlink -e $0))"
 ggo_require_compiler=1
 . $script_dir/../../panda_regressions/hls/generic_getopt.sh
 
-BATCH_ARGS=("--hls-div" "--experimental-setup=BAMBU-PERFORMANCE-MP" "--top-fname=run_benchmark" "--top-rtldesign-name=run_benchmark" "-mx32" "-fno-tree-vectorize" "--simulate" "--simulator=VERILATOR" "-s" "-DBAMBU_PROFILING")
+BATCH_ARGS=("--generate-interface=INFER" "-mx32" "-fno-tree-vectorize" "-s" "--hls-div" "--experimental-setup=BAMBU-PERFORMANCE-MP" "-DCUSTOM_VERIFICATION" "--simulator=VERILATOR" "--simulate")
 OUT_SUFFIX="${compiler}_MachSuite"
+BENCHMARKS_ROOT="${script_dir}/MachSuite"
 
 python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu  \
-   --args="--configuration-name=${compiler} ${BATCH_ARGS[*]}"\
-   -lmachsuite_list \
-   -o "out${OUT_SUFFIX}" -b$script_dir \
+   --args="--configuration-name=${compiler} -I${BENCHMARKS_ROOT}/common --generate-tb=${BENCHMARKS_ROOT}/common/harness.c --generate-tb=${BENCHMARKS_ROOT}/common/support.c ${BATCH_ARGS[*]}"\
+   -l${script_dir}/machsuite_list \
+   -o "out${OUT_SUFFIX}" -b${BENCHMARKS_ROOT} \
    --name="${OUT_SUFFIX}" "$@"
