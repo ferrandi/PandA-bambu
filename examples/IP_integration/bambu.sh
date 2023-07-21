c9ab0a37057d0258ba1152a9a91f0f042347da5c
Michele Fiorito <michele.fiorito@polimi.it>
2023-07-21 13:00:57 +0200

    Examples using top-rtldesign-name update For this example it is now possible to set the correct top interface and exploit the co-simulation for the main execution

--- examples/IP_integration/bambu.sh
+++ examples/IP_integration/bambu.sh
@@ -5,11 +5,11 @@ root_dir=$(dirname $script)
 mkdir -p IP_integration_hls
 cd IP_integration_hls
 echo "#synthesis with VIVADO RTL and simulation"
-timeout 2h bambu $root_dir/main_test.c $root_dir/top.c $root_dir/module_lib.xml --top-fname=main --top-rtldesign-name=my_ip \
-   $root_dir/constraints_STD.xml --experimental-setup=BAMBU --memory-allocation-policy=ALL_BRAM -O3 \
+timeout 2h bambu $root_dir/top.c --top-fname=my_ip $root_dir/module_lib.xml $root_dir/constraints_STD.xml \
+   --experimental-setup=BAMBU --memory-allocation-policy=ALL_BRAM -O3 \
    --C-no-parse=$root_dir/module1.c,$root_dir/module2.c,$root_dir/printer1.c,$root_dir/printer2.c \
    --file-input-data=$root_dir/module1.v,$root_dir/module2.v,$root_dir/printer1.v,$root_dir/printer2.v \
-   --generate-tb=$root_dir/test.xml --simulator=MODELSIM --print-dot --evaluation -DBAMBU_PROFILING "$@"
+   --generate-tb=$root_dir/main_test.c --simulator=MODELSIM --print-dot --evaluation "$@"
 return_value=$?
 if test $return_value != 0; then
    exit $return_value
