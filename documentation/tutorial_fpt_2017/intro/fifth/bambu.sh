8a7082f6b8f18e6a9432bb5c8db53810a211b632
Michele Fiorito <michele.fiorito@polimi.it>
2023-07-21 13:00:57 +0200

    Examples using top-rtldesign-name update For this example it is now possible to set the correct top interface and exploit the co-simulation for the main execution

--- documentation/tutorial_fpt_2017/intro/fifth/bambu.sh
+++ documentation/tutorial_fpt_2017/intro/fifth/bambu.sh
@@ -4,6 +4,6 @@ export PATH=/opt/panda/bin:$PATH
 mkdir -p hls
 cd hls
 echo "# integrating IP simulation"
-bambu --print-dot ../main_test.c ../top.c --generate-tb=../test.xml --C-no-parse=../module1.c,../module2.c,../printer1.c,../printer2.c --file-input-data=../module1.v,../module2.v,../printer1.v,../printer2.v ../module_lib.xml -v4  --experimental-setup=BAMBU --top-fname=main --top-rtldesign-name=my_ip --simulate -DBAMBU_PROFILING ../constraints_STD.xml --memory-allocation-policy=ALL_BRAM -O3  2>&1 | tee integration.log
+bambu --print-dot ../top.c --generate-tb=../main_test.c --C-no-parse=../module1.c,../module2.c,../printer1.c,../printer2.c --file-input-data=../module1.v,../module2.v,../printer1.v,../printer2.v ../module_lib.xml -v4  --experimental-setup=BAMBU --top-fname=my_ip --simulate ../constraints_STD.xml --memory-allocation-policy=ALL_BRAM -O3  2>&1 | tee integration.log
 cd ..
 
--- documentation/tutorial_pact_2019/Introduction/fifth/main_test.c
+++ documentation/tutorial_pact_2019/Introduction/fifth/main_test.c
@@ -1,23 +1,12 @@
 #include "module_lib.h"
 
-#ifdef BAMBU_PROFILING
-extern void __builtin_bambu_time_start();
-extern void __builtin_bambu_time_stop();
-#endif
-
 int main()
 {
-   uint32_t param1=10;
-   uint32_t param2=10<<16;
-#ifdef BAMBU_PROFILING
-   __builtin_bambu_time_start();
-#endif
+   uint32_t param1 = 10;
+   uint32_t param2 = 10 << 16;
    my_ip(0, param1, param2);
    my_ip(1, param1, param2);
    my_ip(2, param1, param2);
    my_ip(3, param1, param2);
-#ifdef BAMBU_PROFILING
-   __builtin_bambu_time_stop();
-#endif
    return 0;
 }
