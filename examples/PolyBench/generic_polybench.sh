d8b38fcb9d04c14e160a67c1fb35159ec643432d
Michele Fiorito <michele.fiorito@polimi.it>
2023-07-23 20:07:48 +0200

    PolyBench examples updated

--- examples/PolyBench/generic_polybench.sh
+++ examples/PolyBench/generic_polybench.sh
@@ -3,13 +3,14 @@ script_dir="$(dirname $(readlink -e $0))"
 ggo_require_compiler=1
 ggo_require_device=1
 ggo_require_period=1
-. $script_dir/../../panda_regressions/hls/generic_getopt.sh
+. ${script_dir}/../../panda_regressions/hls/generic_getopt.sh
 
 BATCH_ARGS=(" --generate-interface=INFER" "--simulate" "-DPOLYBENCH_USE_RESTRICT" "-DPOLYBENCH_STACK_ARRAYS" "-DMINI_DATASET" "-p=__float_mul,__float_add,__float_sub,sqrtf,expf" "-lm" "-s" "--panda-parameter=simple-benchmark-name=1")
 configuration="${device}_$(printf "%04.1f" $period)_$(echo $compiler | tr '[:upper:]' '[:lower:]')"
 OUT_SUFFIX="${configuration}_Polybench"
+BENCHMARKS_ROOT="${script_dir}/PolyBenchC"
 
-python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu  \
+python3 ${script_dir}/../../etc/scripts/test_panda.py --tool=bambu  \
    --args="--configuration-name=${configuration}-float0 -DDATA_TYPE_IS_FLOAT ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${configuration}-float1-dfp -DDATA_TYPE_IS_FLOAT --disable-function-proxy ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${configuration}-float2-no-unroll -DDATA_TYPE_IS_FLOAT --channels-number=1 -fno-unroll-loops ${BATCH_ARGS[*]}"\
@@ -18,8 +19,8 @@ python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu  \
    --args="--configuration-name=${configuration}-float5-no-lp-dfp -DDATA_TYPE_IS_FLOAT --panda-parameter=LP-BB-list=0 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${configuration}-float6-no-unroll-no-lp -DDATA_TYPE_IS_FLOAT --channels-number=1 -fno-unroll-loops --panda-parameter=LP-BB-list=0 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${configuration}-float7-no-unroll-no-lp-dfp -DDATA_TYPE_IS_FLOAT --channels-number=1 -fno-unroll-loops --panda-parameter=LP-BB-list=0 --disable-function-proxy ${BATCH_ARGS[*]}"\
-   -lpolybench_list \
-   -o "out${OUT_SUFFIX}" -b$script_dir \
+   -l${script_dir}/polybench_list \
+   -o "out${OUT_SUFFIX}" -b${BENCHMARKS_ROOT} \
    --name="${OUT_SUFFIX}" "$@"
    
 
