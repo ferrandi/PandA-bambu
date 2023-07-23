ec586a9cf880c197cd0197bdaf9d079759d43e69
Michele Fiorito <michele.fiorito@polimi.it>
2023-07-23 12:29:02 +0200

    CHStone regression scripts fix

--- panda_regressions/hls/generic_CHStone-memarch.sh
+++ panda_regressions/hls/generic_CHStone-memarch.sh
@@ -6,6 +6,7 @@ ggo_require_compiler=1
 
 BATCH_ARGS=("--simulate" "-O3" "-fwhole-program" "--experimental-setup=BAMBU" "--clock-period=15" "-D'printf(fmt, ...)='")
 OUT_SUFFIX="${compiler}_CHStone-memarch"
+BENCHMARKS_ROOT="${script_dir}/../../examples/CHStone/CHStone"
 
 python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu \
    --args="--configuration-name=${compiler}-O3-wp-11-D00-EXT --memory-allocation-policy=EXT_PIPELINED_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
@@ -35,6 +36,6 @@ python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu \
    --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D00-LSS --memory-allocation-policy=LSS --channels-type=MEM_ACC_NN --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D00-GSS --memory-allocation-policy=GSS --channels-type=MEM_ACC_NN --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D00-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
-   -l${script_dir}/chstone_memarch_list \
-   -o "out_${OUT_SUFFIX}" -b${script_dir}/../../examples/CHStone/CHStone \
+   -l${BENCHMARKS_ROOT}/../chstone_list \
+   -o "out_${OUT_SUFFIX}" -b${BENCHMARKS_ROOT} \
    --name="${OUT_SUFFIX}" $ARGS
