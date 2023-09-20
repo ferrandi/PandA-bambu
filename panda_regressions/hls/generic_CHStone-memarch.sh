38b2bd94d0161488f90098229cb4078141e88dd9
Giovanni Gozzi <giovanni.gozzi@mail.polimi.it>
2023-09-20 12:42:45 +0200

    Fixed script for different memory_ctrl testing: removed tests with ALL_BRAM and different memory-ctrl-type as with ALL_BRAM there are no memory_ctrl (and so the tests were the same). Added tests with different memory-ctrl-type for NO_BRAM to test  different kind if MEMORY_CTRL  (BMEMORY_CTRL are aready tested with  EXT_PIPELINED_BRAM)

--- panda_regressions/hls/generic_CHStone-memarch.sh
+++ panda_regressions/hls/generic_CHStone-memarch.sh
@@ -21,21 +21,23 @@ python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu \
    --args="--configuration-name=${compiler}-O3-wp-11-D21-EXT --memory-allocation-policy=EXT_PIPELINED_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${compiler}-O3-wp-N1-D21-EXT --memory-allocation-policy=EXT_PIPELINED_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${compiler}-O3-wp-NN-D21-EXT --memory-allocation-policy=EXT_PIPELINED_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-11-D00-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-N1-D00-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-NN-D00-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-11-D10-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D10 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-N1-D10-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D10 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-NN-D10-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D10 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-11-D11-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D11 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-N1-D11-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D11 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-NN-D11-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D11 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-11-D21-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-N1-D21-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
-   --args="--configuration-name=${compiler}-O3-wp-NN-D21-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-11-D00-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_11 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-N1-D00-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_N1 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-NN-D00-ALL --memory-allocation-policy=ALL_BRAM --channels-type=MEM_ACC_NN ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D00-LSS --memory-allocation-policy=LSS --channels-type=MEM_ACC_NN --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D00-GSS --memory-allocation-policy=GSS --channels-type=MEM_ACC_NN --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-11-D00-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-N1-D00-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
    --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D00-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D00 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-11-D10-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D10 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-N1-D10-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D10 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D10-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D10 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-11-D11-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D11 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-N1-D11-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D11 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D11-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D11 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-11-D21-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_11 --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-N1-D21-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_N1 --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
+   --args="--configuration-name=${compiler}-O3-wp-VVD-NN-D21-NO-BRAM --memory-allocation-policy=NO_BRAM --channels-type=MEM_ACC_NN --memory-ctrl-type=D21 ${BATCH_ARGS[*]}"\
    -l${script_dir}/chstone_memarch_list \
    -o "out_${OUT_SUFFIX}" -b${BENCHMARKS_ROOT} \
    --name="${OUT_SUFFIX}" $ARGS
