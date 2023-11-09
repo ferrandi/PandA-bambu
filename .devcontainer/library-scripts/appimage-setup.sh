13b9e42b58f46873d3f6ddac7243202c5dfa90b9
Michele Fiorito <michele.fiorito@polimi.it>
2023-11-09 09:56:16 +0100

    AppImage launch script fix Reset working directory to the original working directory

--- .devcontainer/library-scripts/appimage-setup.sh
+++ .devcontainer/library-scripts/appimage-setup.sh
@@ -69,6 +69,9 @@ Type=Application
 Terminal=true
 Categories=Development;
 EOF
+# Change back to OWD - Original Working Directory
+# Set by AppImage
+# see https://github.com/AppImage/AppImageKit/blob/master/src/runtime.c
 cat > ${pkg_dir}/usr/bin/tool_select.sh << EOF
 #!/bin/bash
 export LC_ALL="C"
@@ -82,6 +85,7 @@ fi
 if [ ! -e "\$BINARY_PATH" ]; then
    BINARY_PATH="\$APPDIR/usr/bin/bambu"
 fi
+cd "\$OWD"
 \$BINARY_PATH "\$@"
 EOF
 chmod a+x ${pkg_dir}/usr/bin/tool_select.sh
