0ee547b13f545d5be4f210fb7e26fad000f8522c
Michele Fiorito <michele.fiorito@polimi.it>
2023-11-10 19:55:22 +0100

    Fix function pointers example

--- examples/function_pointers/function_pointers.sh
+++ examples/function_pointers/function_pointers.sh
@@ -3,16 +3,16 @@ script_dir="$(dirname $(readlink -e $0))"
 ggo_require_compiler=1
 ggo_require_device=1
 ggo_require_period=1
-. $script_dir/../../panda_regressions/hls/generic_getopt.sh
+. ${script_dir}/../../panda_regressions/hls/generic_getopt.sh
 
 BATCH_ARGS=("--simulate" "--experimental-setup=BAMBU")
 configuration="${device}_$(printf "%04.1f" $period)_$(echo $compiler | tr '[:upper:]' '[:lower:]')"
 OUT_SUFFIX="${configuration}_function_pointers"
 
-python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu \
+python3 ${script_dir}/../../etc/scripts/test_panda.py --tool=bambu \
    --args="--configuration-name=base" \
    --args="--configuration-name=allbram --memory-allocation-policy=ALL_BRAM" \
    --args="--configuration-name=allbram_ntv --memory-allocation-policy=ALL_BRAM -fno-tree-vectorize" \
-   -llist \
-   -o "out${OUT_SUFFIX}" -b$script_dir \
+   -l${script_dir}/function_pointers_list \
+   -o "out${OUT_SUFFIX}" -b${script_dir} \
    --name="${OUT_SUFFIX}" "$@"
