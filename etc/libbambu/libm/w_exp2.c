1447a4ad22b6835a3998aa9d402b095e9e824246
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2023-12-06 10:11:28 +0100

    Added support to double precision exp2 libm function.

---
+++ etc/libbambu/libm/w_exp2.c
@@ -0,0 +1,27 @@
+/**
+ * Porting of the libm library to the PandA framework
+ * starting from the original FDLIBM 5.3 (Freely Distributable LIBM) developed by SUN
+ * plus the newlib version 1.19 from RedHat and plus uClibc version 0.9.32.1 developed by Erik Andersen.
+ * The author of this port is Fabrizio Ferrandi from Politecnico di Milano.
+ * The porting fall under the LGPL v2.1, see the files COPYING.LIB and COPYING.LIBM_PANDA in this directory.
+ * Date: December, 6 2023.
+ */
+/*
+ * ====================================================
+ * Copyright (C) 2004 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice
+ * is preserved.
+ * ====================================================
+ */
+
+#include "math_private.h"
+
+/*
+ * wrapper exp2(x)
+ */
+double exp2(double x) /* wrapper exp */
+{
+  return pow(2.0, x);
+}
