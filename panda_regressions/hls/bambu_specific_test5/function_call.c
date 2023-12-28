57d225beaf59a0182376a306c40b0485e3b7b63f
Michele Fiorito <michele.fiorito@polimi.it>
2023-12-28 11:57:10 +0100

    Fix HLS pipeline pragma parser

--- panda_regressions/hls/bambu_specific_test5/function_call.c
+++ panda_regressions/hls/bambu_specific_test5/function_call.c
@@ -1,20 +1,18 @@
 int __attribute__((noinline)) called_sumprod(int a, int b, int c, int d)
 {
+#pragma HLS pipeline style = frp
 
-   #pragma HLS pipeline
-   
    return a + b + c * d;
 }
 
 int main_caller(int a, int b, int c, int d, int e)
 {
+#pragma HLS pipeline
 
-   #pragma HLS pipeline
-   
    int aa = called_sumprod(a, b, c, d);
    int ab = called_sumprod(b, c, d, e);
    int ac = called_sumprod(c, d, e, a);
    int ad = called_sumprod(d, e, a, b);
-   
+
    return called_sumprod(aa, ab, ac, ad);
 }
