48bd70d9650c748b6a04ac2a002c4073f4cc6315
Michele Fiorito <michele.fiorito@polimi.it>
2024-02-06 00:57:12 +0100

    Update bambu specific 4 test cases

--- panda_regressions/hls/bambu_specific_test4.sh
+++ panda_regressions/hls/bambu_specific_test4.sh
@@ -4,7 +4,10 @@ BATCH_ARGS=("--simulate" "--generate-interface=INFER")
 OUT_SUFFIX="bambu_specific_test4"
 
 python3 $script_dir/../../etc/scripts/test_panda.py --tool=bambu  \
-   --args="--configuration-name=BALANCED_MP_CLANG6 --compiler=I386_CLANG6 --experimental-setup=BAMBU-BALANCED-MP ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=BASE_CLANG6  --compiler=I386_CLANG6  --experimental-setup=BAMBU ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=BASE_CLANG16 --compiler=I386_CLANG16 --experimental-setup=BAMBU ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=BALANCED_MP_CLANG6  --compiler=I386_CLANG6  --experimental-setup=BAMBU-BALANCED-MP ${BATCH_ARGS[*]}" \
+   --args="--configuration-name=BALANCED_MP_CLANG16 --compiler=I386_CLANG16 --experimental-setup=BAMBU-BALANCED-MP ${BATCH_ARGS[*]}" \
    -lbambu_specific_test4_list \
    -o "out_${OUT_SUFFIX}" -b$script_dir \
    --name="${OUT_SUFFIX}" "$@"
