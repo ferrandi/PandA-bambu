e1c32952075ec726880e8c1c366e7a845b2779ee
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2024-03-08 12:45:05 +0100

    Clang format applied.

--- src/frontend_analysis/IR_analysis/call_graph_computation.cpp
+++ src/frontend_analysis/IR_analysis/call_graph_computation.cpp
@@ -115,7 +115,7 @@ DesignFlowStep_Status call_graph_computation::Exec()
    CustomSet<unsigned int> functions;
    INDENT_DBG_MEX(DEBUG_LEVEL_VERY_PEDANTIC, debug_level, "---Top functions passed by user");
    auto function_symbols = parameters->getOption<std::vector<std::string>>(OPT_top_functions_names);
-   ///checking if the top functions are present in the IR
+   /// checking if the top functions are present in the IR
    for(const auto& symbol : function_symbols)
    {
       const auto fnode = TM->GetFunction(symbol);
