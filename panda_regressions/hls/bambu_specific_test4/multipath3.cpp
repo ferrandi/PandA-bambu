f544c87d0dc30f2ca7adbbb09b01163051b4bb75
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2024-03-12 17:44:09 +0100

    Added a missing file.

---
+++ panda_regressions/hls/bambu_specific_test4/multipath3.cpp
@@ -0,0 +1,48 @@
+#include "dataflow2.hpp"
+
+#include "AddBlock.hpp"
+#include "MulBlock.hpp"
+#include "SumBlock.hpp"
+
+using namespace test;
+
+test::MulBlock::MulBlock()
+{
+}
+
+void test::MulBlock::compute(ac_channel<int>& in, ac_channel<int>& out)
+{
+   int val = in.read();
+   int i = 0;
+   for(; i < 3; ++i)
+      val += val;
+   out.write(val);
+}
+
+class SimpleSystem
+{
+   AddBlock<5> a5;
+   AddBlock<7> a7;
+   SumBlock<int> s;
+   MulBlock m1;
+
+   ac_channel<int> x, y, z;
+
+ public:
+   void top(ac_channel<int>& in1, ac_channel<int>& in2, ac_channel<int>& out)
+   {
+#pragma HLS dataflow
+      a5.compute(in1, x);
+      m1.compute(x, y);
+
+      a7.compute(in2, z);
+
+      s.compute(y, z, out);
+   }
+};
+
+void dataflow_top(ac_channel<int>& in1, ac_channel<int>& in2, ac_channel<int>& out)
+{
+   SimpleSystem sys;
+   sys.top(in1, in2, out);
+}
