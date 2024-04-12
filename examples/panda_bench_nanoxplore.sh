48d937b8a6cd9ff784aa51d0f8d361d1de6c20ce
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2024-04-12 19:03:58 +0200

    Fixed the regression scripts defaults.

--- examples/panda_bench_nanoxplore.sh
+++ examples/panda_bench_nanoxplore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_dir="$(dirname $(readlink -e $0))"
-BATCH_ARGS=("--clock-period=20" "--evaluation")
+BATCH_ARGS=("--clock-period=20" "--evaluation" "--compiler=I386_GCC49")
 OUT_SUFFIX="pb_nanoxplore"
 
 python3 $script_dir/../etc/scripts/test_panda.py --tool=bambu  \
