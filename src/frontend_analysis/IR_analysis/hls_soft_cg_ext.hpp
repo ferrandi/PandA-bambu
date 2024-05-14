d9f042508ce018f0b50edf843f6fd85d98f252d2
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2024-05-14 11:02:32 +0200

    Added two missing files.

---
+++ src/frontend_analysis/IR_analysis/hls_soft_cg_ext.hpp
@@ -0,0 +1,109 @@
+/*
+ *
+ *                   _/_/_/    _/_/   _/    _/ _/_/_/    _/_/
+ *                  _/   _/ _/    _/ _/_/  _/ _/   _/ _/    _/
+ *                 _/_/_/  _/_/_/_/ _/  _/_/ _/   _/ _/_/_/_/
+ *                _/      _/    _/ _/    _/ _/   _/ _/    _/
+ *               _/      _/    _/ _/    _/ _/_/_/  _/    _/
+ *
+ *             ***********************************************
+ *                              PandA Project
+ *                     URL: http://panda.dei.polimi.it
+ *                       Politecnico di Milano - DEIB
+ *                        System Architectures Group
+ *             ***********************************************
+ *              Copyright (C) 2004-2024 Politecnico di Milano
+ *
+ *   This file is part of the PandA framework.
+ *
+ *   The PandA framework is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/**
+ * @file hls_soft_cg_ext.hpp
+ * @brief Step that extends the call graph with the division and modulus function calls where appropriate.
+ *
+ * @author Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
+ *
+ */
+#ifndef HLS_DIV_CG_EXT_HPP
+#define HLS_DIV_CG_EXT_HPP
+
+/// Superclass include
+#include "function_frontend_flow_step.hpp"
+
+/// Utility include
+#include "custom_set.hpp"
+#include "refcount.hpp"
+
+/**
+ * @name forward declarations
+ */
+//@{
+REF_FORWARD_DECL(hls_soft_cg_ext);
+REF_FORWARD_DECL(tree_manager);
+REF_FORWARD_DECL(tree_manipulation);
+REF_FORWARD_DECL(tree_node);
+//@}
+
+/**
+ * Add to the call graph the function calls associated with the integer division and modulus operations.
+ */
+class hls_soft_cg_ext : public FunctionFrontendFlowStep
+{
+ private:
+   /// Already visited tree node (used to avoid infinite recursion)
+   CustomUnorderedSet<unsigned int> already_visited;
+
+   const tree_managerRef TreeM;
+
+   bool use64bitMul;
+   bool use32bitMul;
+   bool doSoftDiv;
+
+   /**
+    * Recursive examine tree node
+    */
+   bool recursive_examinate(const tree_nodeRef& current_tree_node, const tree_nodeRef& current_statement,
+                            const tree_manipulationRef tree_man);
+
+   /**
+    * Return the set of analyses in relationship with this design step
+    * @param relationship_type is the type of relationship to be considered
+    */
+   const CustomUnorderedSet<std::pair<FrontendFlowStepType, FunctionRelationship>>
+   ComputeFrontendRelationships(const DesignFlowStep::RelationshipType relationship_type) const override;
+
+ public:
+   /**
+    * Constructor.
+    * @param Param is the set of the parameters
+    * @param AppM is the application manager
+    * @param fun_id is the function index
+    * @param design_flow_manager is the design flow manager
+    */
+   hls_soft_cg_ext(const ParameterConstRef _parameters, const application_managerRef AppM, unsigned int _function_id,
+                  const DesignFlowManagerConstRef design_flow_manager);
+
+   /**
+    * Destructor
+    */
+   ~hls_soft_cg_ext() override;
+
+   /**
+    * Fixes the var_decl duplication.
+    */
+   DesignFlowStep_Status InternalExec() override;
+};
+#endif
