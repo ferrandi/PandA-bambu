ff1b017b1a0d52b698f1d5a0cc32b825565a9ca1
giovanni gozzi <giovanni.gozzi@polimi.it>
2025-06-17 17:05:41 +0200

    Updated tutorial bambu101: - Added testbench to basic_usage section - Included examples of AXI4 and cache pragmas

---
+++ documentation/bambu101/Axi4/Exercise4/mmult.h
@@ -0,0 +1,17 @@
+#define rank 32
+#define tile_rank 2
+
+/* AXI pragmas */
+#pragma HLS interface port = a mode = m_axi offset = direct bundle = gmem0
+#pragma HLS interface port = b mode = m_axi offset = direct bundle = gmem1
+#pragma HLS interface port = c mode = m_axi offset = direct bundle = gmem2
+
+/* Cache pragmas */
+#pragma HLS cache bundle = gmem0 line_count = 16 line_size = 16 bus_size = 32 ways = 1 num_write_outstanding = \
+    2 rep_policy = lru write_policy = wt
+#pragma HLS cache bundle = gmem1 line_count = 32 line_size = 16 bus_size = 32 ways = 1 num_write_outstanding = \
+    2 rep_policy = tree write_policy = wt
+#pragma HLS cache bundle = gmem2 line_count = 16 line_size = 16 bus_size = 32 ways = 1 num_write_outstanding = \
+    4 rep_policy = tree write_policy = wb
+
+void mmult(int* a, int* b, int* c);
---
+++ documentation/bambu101/Axi4/Exercise5/mmult.c
@@ -0,0 +1,51 @@
+#define rank 32
+#define tile_rank 2
+
+/* AXI pragmas */
+#pragma HLS interface port = a mode = m_axi offset = direct bundle = gmem0
+#pragma HLS interface port = b mode = m_axi offset = direct bundle = gmem1
+#pragma HLS interface port = c mode = m_axi offset = direct bundle = gmem2
+
+/* Cache pragmas */
+#pragma HLS cache bundle = gmem0 line_count = 16 line_size = 16 bus_size = 32 ways = 1 num_write_outstanding = \
+    2 rep_policy = lru write_policy = wt
+#pragma HLS cache bundle = gmem1 line_count = 32 line_size = 16 bus_size = 32 ways = 1 num_write_outstanding = \
+    2 rep_policy = tree write_policy = wt
+#pragma HLS cache bundle = gmem2 line_count = 16 line_size = 16 bus_size = 32 ways = 1 num_write_outstanding = \
+    4 rep_policy = tree write_policy = wb
+
+void mmult(int* a, int* b, int* c)
+{
+   int running = 0;
+
+   for(unsigned c_tile = 0; c_tile < tile_rank; c_tile++)
+   {
+      for(unsigned r_tile = 0; r_tile < tile_rank; r_tile++)
+      {
+         for(unsigned r = 0; r < rank / tile_rank; r++)
+         {
+            for(unsigned c = 0; c < rank / tile_rank; c++)
+            {
+               c[(r + r_tile * rank / tile_rank) * rank + (c + c_tile * rank / tile_rank)] = 0;
+            }
+         }
+         for(unsigned i_tile = 0; i_tile < tile_rank; i_tile++)
+         {
+            for(unsigned c = 0; c < rank / tile_rank; c++)
+            {
+               for(unsigned r = 0; r < rank / tile_rank; r++)
+               {
+                  running = 0;
+                  for(unsigned index = 0; index < rank / tile_rank; index++)
+                  {
+                     unsigned aIndex = (r + r_tile * rank / tile_rank) * rank + (index + i_tile * rank / tile_rank);
+                     unsigned bIndex = (index + i_tile * rank / tile_rank) * rank + (c + c_tile * rank / tile_rank);
+                     running += a[aIndex] * b[bIndex];
+                  }
+                  c[(r + r_tile * rank / tile_rank) * rank + (c + c_tile * rank / tile_rank)] += running;
+               }
+            }
+         }
+      }
+   }
+}
