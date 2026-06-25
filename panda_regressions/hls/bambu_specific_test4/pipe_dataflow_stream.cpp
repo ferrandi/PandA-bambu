39ecc1006a1e52cb9418b55237438e929a3b771e
Fabrizio Ferrandi. <fabrizio.ferrandi@polimi.it>
2026-06-25 14:05:08 +0200

    Improved pipe dataflow stream example.

--- panda_regressions/hls/bambu_specific_test4/pipe_dataflow_stream.cpp
+++ panda_regressions/hls/bambu_specific_test4/pipe_dataflow_stream.cpp
@@ -1,51 +1,68 @@
-#include <hls_stream.h>
 #include <cstdio>
+#include <hls_stream.h>
 
+#define BATCH_ITEM_NUMBER 16
+#define TOTAL_BATCHES 8
 
-extern "C" void __attribute__((noinline)) load(hls::stream<unsigned>& dma_in, hls::stream<unsigned char>& ctrl_out, unsigned int array_out[16*2])
+extern "C" void __attribute__((noinline))
+load(hls::stream<unsigned>& dma_in, hls::stream<unsigned char>& ctrl_out, unsigned int array_out[BATCH_ITEM_NUMBER * 2])
 {
    static unsigned char out_buffer_index = 0;
-   for(int i = 0; i < 16; ++i)
+   for(int b = 0; b < TOTAL_BATCHES; ++b)
    {
-      array_out[16*out_buffer_index+i] = dma_in.read();
+      for(int i = 0; i < BATCH_ITEM_NUMBER; ++i)
+      {
+         array_out[BATCH_ITEM_NUMBER * out_buffer_index + i] = dma_in.read();
+      }
+      ctrl_out.write(out_buffer_index);
+      out_buffer_index = (out_buffer_index + 1) % 2;
    }
-   ctrl_out.write(out_buffer_index);
-   out_buffer_index =  (out_buffer_index + 1)%2;
 }
 
-extern "C" void __attribute__((noinline)) compute(hls::stream<unsigned char>& ctrl_in, hls::stream<unsigned char>& ctrl_out, unsigned int array_in[16*2], unsigned int array_out[16*2])
+extern "C" void __attribute__((noinline))
+compute(hls::stream<unsigned char>& ctrl_in, hls::stream<unsigned char>& ctrl_out,
+        unsigned int array_in[BATCH_ITEM_NUMBER * 2], unsigned int array_out[BATCH_ITEM_NUMBER * 2])
 {
    static unsigned char out_buffer_index = 0;
-   unsigned char in_buffer_index = ctrl_in.read();
-   for(int i = 0; i < 16; ++i)
+   for(int b = 0; b < TOTAL_BATCHES; ++b)
    {
-      array_out[16*out_buffer_index+i] = array_in[16*in_buffer_index+i] * array_in[16*in_buffer_index+i];
+      unsigned char in_buffer_index = ctrl_in.peek();
+      for(int i = 0; i < BATCH_ITEM_NUMBER; ++i)
+      {
+         array_out[BATCH_ITEM_NUMBER * out_buffer_index + i] =
+             array_in[BATCH_ITEM_NUMBER * in_buffer_index + i] * array_in[BATCH_ITEM_NUMBER * in_buffer_index + i];
+      }
+      ctrl_in.read();
+      ctrl_out.write(out_buffer_index);
+      out_buffer_index = (out_buffer_index + 1) % 2;
    }
-   ctrl_out.write(out_buffer_index);
-   out_buffer_index =  (out_buffer_index + 1)%2;
 }
 
-
-extern "C" void __attribute__((noinline)) store(hls::stream<unsigned char>& ctrl_in, unsigned int array_in[16*2], hls::stream<unsigned>& dma_out)
+extern "C" void __attribute__((noinline))
+store(hls::stream<unsigned char>& ctrl_in, unsigned int array_in[BATCH_ITEM_NUMBER * 2], hls::stream<unsigned>& dma_out)
 {
-   unsigned char in_buffer_index = ctrl_in.read();
-   for(int i = 0; i < 16; ++i)
+   for(int b = 0; b < TOTAL_BATCHES; ++b)
    {
-      dma_out.write(array_in[16*in_buffer_index+i]);
+      unsigned char in_buffer_index = ctrl_in.peek();
+      for(int i = 0; i < BATCH_ITEM_NUMBER; ++i)
+      {
+         dma_out.write(array_in[BATCH_ITEM_NUMBER * in_buffer_index + i]);
+      }
+      ctrl_in.read();
    }
 }
 
 #pragma HLS interface port = dma_in mode = axis
 #pragma HLS interface port = dma_out mode = axis
-extern "C" void top(hls::stream<unsigned>&dma_in, hls::stream<unsigned>&dma_out)
+extern "C" void top(hls::stream<unsigned>& dma_in, hls::stream<unsigned>& dma_out)
 {
-   #pragma HLS DATAFLOW
+#pragma HLS DATAFLOW
 
    hls::stream<unsigned char> ctrl1;
    hls::stream<unsigned char> ctrl2;
-   unsigned int buffer1[16*2];
-   unsigned int buffer2[16*2];
+   unsigned int buffer1[BATCH_ITEM_NUMBER * 2];
+   unsigned int buffer2[BATCH_ITEM_NUMBER * 2];
    load(dma_in, ctrl1, buffer1);
    compute(ctrl1, ctrl2, buffer1, buffer2);
-   store(ctrl2,buffer2,dma_out);
+   store(ctrl2, buffer2, dma_out);
 }
--- panda_regressions/hls/bambu_specific_test4/pipe_dataflow_stream_tb.cpp
+++ panda_regressions/hls/bambu_specific_test4/pipe_dataflow_stream_tb.cpp
@@ -1,18 +1,20 @@
-#include <hls_stream.h>
 #include <cstdio>
-#ifdef  __BAMBU__
+#include <hls_stream.h>
+#ifdef __BAMBU__
 #include <mdpi/mdpi_user.h>
 #endif
 
+#define BATCH_ITEM_NUMBER 16
+#define TOTAL_BATCHES 8
 
-extern "C" void top(hls::stream<unsigned>&dma_in, hls::stream<unsigned>&dma_out);
+extern "C" void top(hls::stream<unsigned>& dma_in, hls::stream<unsigned>& dma_out);
 
 int main()
 {
    hls::stream<unsigned> inputData, outputData;
 
    // Initialise input array with values 1..16
-   for(int j = 0; j < 2; j++)
+   for(int j = 0; j < TOTAL_BATCHES; j++)
       for(int i = 0; i < 16; i++)
          inputData.write((unsigned long long)(i + 1));
 
@@ -21,17 +23,17 @@ int main()
    top(inputData, outputData);
 
    int pass = 1;
-   for(int j = 0; j < 2; j++)
+   for(int j = 0; j < TOTAL_BATCHES; j++)
    {
-      for(int i = 0; i < 16; i++)
+      for(int i = 0; i < BATCH_ITEM_NUMBER; i++)
       {
-        unsigned expected = (unsigned long long)(i + 1) * (unsigned long long)(i + 1);
-        unsigned computed = outputData.read();
-        if(computed != expected)
-        {
-           printf("FAIL at [%d]: got %u, expected %u\n", i, computed, expected);
-           pass = 0;
-        }
+         unsigned expected = (unsigned long long)(i + 1) * (unsigned long long)(i + 1);
+         unsigned computed = outputData.read();
+         if(computed != expected)
+         {
+            printf("FAIL at [%d]: got %u, expected %u\n", i, computed, expected);
+            pass = 0;
+         }
       }
    }
 
